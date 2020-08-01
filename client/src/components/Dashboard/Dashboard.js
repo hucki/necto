2c4efbd405d1af69a67af2b88193e553b4305db2
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-01 09:33:30 +0200

    refactor: (client) remove unused components, AppointmentInputForm+Item, CalendarDay+Hour+Container etc

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import classes from './Dashboard.module.css';
-import Calendar from '../Calendar/Calendar';
 import TimetableContainer from '../Timetable/TimetableContainer/TimetableContainer';
 
 
