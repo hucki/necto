0446d369550ab68f248440db743f0d3a995f69ba
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-01 09:49:42 +0200

    chore: add eslint and setup rules

---
+++ client/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": ["error", 2, { "SwitchCase": 1 }],
+        "keyword-spacing": "error",
+        "linebreak-style": ["error", "unix"],
+        "no-undef": 0,
+        "no-unsued-vars": 0,
+        "quotes": ["error", "single"],
+        "react/prop-types": 0,
+        "react/display-name": 0,
+        "semi": ["error", "always"],
+        "space-before-blocks": "error",
+        "space-before-function-paren": "error"
+    }
+};
