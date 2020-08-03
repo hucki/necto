d0e58893b9e9c843b959883707ef5a0cf16e9aaf
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-03 23:17:25 +0200

    feat: (client) restore calculation of clicked Time and Functionality of InputForm

--- client/src/components/Timetable/TeamtableContainer/TeamtableContainer.js
+++ client/src/components/Timetable/TeamtableContainer/TeamtableContainer.js
@@ -24,11 +24,12 @@ const TeamtableContainer = ({events, currentDate, hoursInterval, dispatch, visib
     width: -1,
     height: -1,
   }});
-  const { width, height } = dimensions.dimensions
+  const { width, height, top } = dimensions.dimensions
   const [numOfHours, setNumOfHours] = useState(hoursInterval[1]-hoursInterval[0]+1)
-  const [numOfRows, setNumOfRows] = useState(Object.keys(events).length + 1);
+  const [numOfCols, setNumOfCols] = useState(Object.keys(events).length);
+  const [timeScaleWidth, setTimeScaleWidth] = useState(50);
   const [cellHeight, setCellHeight] = useState(height/numOfHours);
-  const [cellWidth, setCellWidth] = useState(width/numOfRows);
+  const [cellWidth, setCellWidth] = useState((width-timeScaleWidth)/numOfCols);
   const [cellStyle, setCellStyle] = useState({height: `${cellHeight}px`, width: `${cellWidth}px`})
   const [startOfDay, setStartOfDay] = useState(currentDate.clone().set('hour', hoursInterval[0]).set('minutes', 0).set('seconds', 0))
 
@@ -39,59 +40,65 @@ const TeamtableContainer = ({events, currentDate, hoursInterval, dispatch, visib
 
   function calcDimensions() {
     setCellHeight(height/numOfHours);
-    setCellWidth(width/numOfRows);
+    setCellWidth((width-timeScaleWidth)/numOfCols);
     setCellStyle({height: `${cellHeight}px`, width: `${cellWidth}px`});
-    console.log(dimensions)
   }
 
   function getPosition (e) {
+    e.preventDefault();
 
-  //   e.preventDefault();
-  //   if((typeof e.target.className) !== 'string') return;
-  //   const clickOnFreeTime = !e.target.className.indexOf('styles_day__');
-  //   if (clickOnFreeTime) {
-  //     dispatch(clickRow(e.target.className.split(' ')[1]));
-  //     const rect = e.target.getBoundingClientRect();
-  //     const y = e.clientY - rect.top;
-  //     const numOfHours = hoursInterval[1]-hoursInterval[0]+1;
-  //     const cellHeight = (rect.bottom-rect.top)/numOfHours;
-  //     const startOfDay = currentDate.clone().set('hour', hoursInterval[0]).set('minutes', 0).set('seconds', 0);
-  //     const clickTimeMin = ((y-cellHeight)/(cellHeight/60));
-  //     const clickTime = startOfDay.clone().add(clickTimeMin,'m');
-  //     const fullQuarterHour = Math.round(clickTime.get('minute')/15)*15;
-  //     const clickTimeFQH = clickTime.set('minutes', 0).add(fullQuarterHour, 'm').set('seconds',0);
+    if((typeof e.target.className) !== 'string') return;
+    const clickOnFreeTime = !e.target.className.indexOf('TeamtableContainer_day__');
 
-  //     dispatch(setStart(clickTimeFQH));
-  //     dispatch(setEnd(clickTimeFQH.add(45,'m')));
-  //     dispatch(toggleVisible());
-  //   }
+    if (clickOnFreeTime) {
+      dispatch(clickRow(e.target.className.split(' ')[1]));
+      //     const rect = e.target.getBoundingClientRect();
+      const y = e.clientY - top;
+      //     const numOfHours = hoursInterval[1]-hoursInterval[0]+1;
+      //     const cellHeight = (rect.bottom-rect.top)/numOfHours;
+      //     const startOfDay = currentDate.clone().set('hour', hoursInterval[0]).set('minutes', 0).set('seconds', 0);
+      const clickTimeMin = ((y-cellHeight)/(cellHeight/60));
+      const clickTime = startOfDay.clone().add(clickTimeMin,'m').set('seconds',0);
+      const fullQuarterHour = Math.round(clickTime.get('minute')/15)*15;
+      const clickTimeFQH = clickTime.set('minutes', 0).add(fullQuarterHour, 'm');
+
+      dispatch(setStart(clickTimeFQH));
+      dispatch(setEnd(clickTimeFQH.add(45,'m')));
+      dispatch(toggleVisible());
+    }
   }
   const hoursScale = Array(hoursInterval[1]-hoursInterval[0]).fill(null).map((hour, index) =>
     <div  key={index+hoursInterval[0]}
-          style={cellStyle}> {index+hoursInterval[0]}:00</div>);
+          className={classes.hour}
+          style={{height: cellHeight, width: `${timeScaleWidth}px`}}> {index+hoursInterval[0]}:00</div>);
 
   const days = Object.keys(events).map(personId =>
-    <div key={personId} className={classes.day} style={{
-      height: `100%`,
-      width: `${cellWidth}px`,
-      backgroundSize: '1px ' + 2 * cellHeight + 'px',
-      }}>
-        <div className={classes.dayHeader}>{personId}</div>
+    <div  key={personId}
+          className={`${classes.day} ${personId}`}
+          style={{
+            height: `100%`,
+            width: `${cellWidth}px`,
+            backgroundSize: '1px ' + 2 * cellHeight + 'px',
+          }}>
+        <div className={classes.dayHeader} style={{
+      height: `${cellHeight}px`,
+      width: `${cellWidth}px`}}>{personId}</div>
         {events[personId].map(event => renderCustomEvent(event))}
     </div>);
 
   return (
+    <>
     <Measure
       bounds
       onResize={contentRect => {
       setDimensions({ dimensions: contentRect.bounds });
       setCellHeight(height/numOfHours);
-      setCellWidth(width/numOfRows);
+      setCellWidth(width/numOfCols);
     }}>
       {({ measureRef }) => (
-          <div className={classes.TeamtableContainer} onClick={getPosition} ref={measureRef}>
-            <div className='teamscale'>
-              <div className={classes.hoursScaleHeader} style={cellStyle}> Time</div>
+          <div className={classes.TeamtableContainer} onClick={getPosition} ref={measureRef} style={{backgroundSize: '1px ' + 2 * cellHeight + 'px'}}>
+            <div className={classes.hoursScale}>
+              <div className={classes.hoursScaleHeader} style={{height: cellStyle.height, width: `${timeScaleWidth}px`}}> Time</div>
               {hoursScale}
             </div>
             {days}
@@ -114,6 +121,8 @@ const TeamtableContainer = ({events, currentDate, hoursInterval, dispatch, visib
         <TimetableInputForm visible={visible}/>
       </div> */}
     </Measure>
+    <TimetableInputForm visible={visible}/>
+    </>
   );
 };
 
