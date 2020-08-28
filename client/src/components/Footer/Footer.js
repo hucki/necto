10aca16b26e9dc2158ac666748fe8900ac03d7cb
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-28 22:12:32 +0200

    refactor(footer): add accessible label

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import classes from './Footer.module.css';
 
-function Footer () {
+function Footer() {
   return (
     <div className={classes.Footer}>
-      <p>made with 🍍🍕 by Hucki</p>
+      <p>
+        made with{' '}
+        <span role="img" aria-label="pineapple">
+          🍍
+        </span>
+        <span role="img" aria-label="pizza">
+          🍕
+        </span>{' '}
+        by Hucki
+      </p>
     </div>
   );
 }
