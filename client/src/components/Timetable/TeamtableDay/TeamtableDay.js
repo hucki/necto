03be68d65e93e2782e8206ba5ffeb1933677f643
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-28 22:15:54 +0200

    refactor(TeamtbaleDay): remove unused variables

--- client/src/components/Timetable/TeamtableDay/TeamtableDay.js
+++ client/src/components/Timetable/TeamtableDay/TeamtableDay.js
@@ -3,59 +3,65 @@ import { connect } from 'react-redux';
 import classes from './TeamtableDay.module.css';
 import TeamTableItem from '../TeamTableItem/TeamTableItem';
 import dayjs from 'dayjs';
-import { Progress } from 'antd';
 
-const TeamtableDay = ({events, headerArray, cellWidth, cellHeight, hoursInterval}) => {
-
+const TeamtableDay = ({
+  events,
+  headerArray,
+  cellWidth,
+  cellHeight,
+  hoursInterval,
+}) => {
   const renderCustomEvent = (event, styles) => {
-    return (
-      <TeamTableItem
-        key={event.id}
-        event={event}
-        styles={styles}/>
-    );
+    return <TeamTableItem key={event.id} event={event} styles={styles} />;
   };
   function getItemStyle(event) {
-    const minsFromStartOfDay =  dayjs(event.startTime).diff(dayjs(event.startTime).startOf('day').add(hoursInterval[0],'h'), 'minute');
+    const minsFromStartOfDay = dayjs(event.startTime).diff(
+      dayjs(event.startTime).startOf('day').add(hoursInterval[0], 'h'),
+      'minute'
+    );
     const minsDuration = dayjs(event.endTime).diff(event.startTime, 'minute');
     const pxPerMinute = cellHeight / 60;
-    const pxFromStartOfDay = (pxPerMinute * minsFromStartOfDay) + cellHeight;
+    const pxFromStartOfDay = pxPerMinute * minsFromStartOfDay + cellHeight;
     const pxItemHeight = pxPerMinute * minsDuration;
     const itemStyle = {
       top: `${pxFromStartOfDay}px`,
-      height: `${pxItemHeight}px`
+      height: `${pxItemHeight}px`,
     };
-    // console.log(cellHeight, minsFromStartOfDay, minsDuration, parseInt(pxPerMinute) ,parseInt(pxFromStartOfDay))
     return itemStyle;
   }
 
-  const numOfHours = hoursInterval[1]-hoursInterval[0]+1;
-  const relCellHeightStyle = {height: `${100/numOfHours}%`};
+  const numOfHours = hoursInterval[1] - hoursInterval[0] + 1;
+  const relCellHeightStyle = { height: `${100 / numOfHours}%` };
 
-  const days = headerArray.map((rowTitle, index) =>
-    <div  key={rowTitle}
-          id={index}
-          className={`${classes.day} ${rowTitle}`}
-          style={{
-            height: `100%`,
-            width: `${cellWidth}px`,
-          }}>
-        <div className={classes.dayHeader} style={{
-      ...relCellHeightStyle,
-      width: `${cellWidth}px`}}>
+  const days = headerArray.map((rowTitle, index) => (
+    <div
+      key={rowTitle}
+      id={index}
+      className={`${classes.day} ${rowTitle}`}
+      style={{
+        height: `100%`,
+        width: `${cellWidth}px`,
+      }}
+    >
+      <div
+        className={classes.dayHeader}
+        style={{
+          ...relCellHeightStyle,
+          width: `${cellWidth}px`,
+        }}
+      >
         <div className={classes.dayHeaderText}>{rowTitle} </div>
       </div>
-      {events[rowTitle].map(event => renderCustomEvent(event, getItemStyle(event)))}
-    </div>);
+      {events[rowTitle].map((event) =>
+        renderCustomEvent(event, getItemStyle(event))
+      )}
+    </div>
+  ));
 
-  return (
-    <>
-      {days}
-    </>
-  )
-}
+  return <>{days}</>;
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     hoursInterval: state.settings.hoursInterval,
     cellWidth: state.teamtable.calculatedDimensions.cellWidth,
