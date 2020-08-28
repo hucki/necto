4ca6e37645d777dd556218455c9034ec84acea75
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-28 22:58:54 +0200

    refactor(TeamtableItem): add event to dependencies array

--- client/src/components/Timetable/TeamTableItem/TeamTableItem.js
+++ client/src/components/Timetable/TeamTableItem/TeamTableItem.js
@@ -1,34 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import classes from '../Teamtable.module.css';
-import {Modal, message} from 'antd';
+import { Modal, message } from 'antd';
 import dayjs from 'dayjs';
 import { deleteAppointment } from '../../../actions/actions';
 import { HomeTwoTone, ApiTwoTone } from '@ant-design/icons';
 
-const TeamtableItem = ({event, styles,  dispatch}) => {
+const TeamtableItem = ({ event, styles, dispatch }) => {
   // TODO: put delete dialog in a separate Component
   const { confirm } = Modal;
   const [icons, setIcons] = useState([]);
 
   useEffect(() => {
-    if (event.rrule !== '') setIcons(icons => [...icons, <ApiTwoTone key='rruleIcon'/>]);
-    if (event.homeVisit) setIcons(icons => [...icons, <HomeTwoTone key='homeVisitIcon'/>]);
-  }, [])
+    if (event.rrule !== '')
+      setIcons((icons) => [...icons, <ApiTwoTone key="rruleIcon" />]);
+    if (event.homeVisit)
+      setIcons((icons) => [...icons, <HomeTwoTone key="homeVisitIcon" />]);
+  }, [event]);
 
-  const onClickHandler = ({id, name, startTime, endTime}) => {
+  const onClickHandler = ({ id, name, startTime, endTime }) => {
     confirm({
-      content: (<div>
-        <p>Delete Appointment id: {id}?</p>
-        <p>{dayjs(startTime).format('HH:mm')} - {dayjs(endTime).format('HH:mm')}: <b>{name}</b></p>
-      </div>),
-      onOk () {
+      content: (
+        <div>
+          <p>Delete Appointment id: {id}?</p>
+          <p>
+            {dayjs(startTime).format('HH:mm')} -{' '}
+            {dayjs(endTime).format('HH:mm')}: <b>{name}</b>
+          </p>
+        </div>
+      ),
+      onOk() {
         dispatch(deleteAppointment(id));
-        message.success(`Appointment ${id} deleted`,1);
+        message.success(`Appointment ${id} deleted`, 1);
       },
-      onCancel () {
+      onCancel() {
         console.log('delete process cancelled'); // eslint-disable-line no-console
-      }
+      },
     });
   };
 
@@ -41,22 +48,20 @@ const TeamtableItem = ({event, styles,  dispatch}) => {
       className={`${classes.event} ${classes[event.bgcolor]}`}
     >
       <div className={classes.event_container}>
-
-        <span className={classes.event_info}>{ event.name }</span>
+        <span className={classes.event_info}>{event.name}</span>
         <div className={classes.icon_container}>{icons}</div>
       </div>
       <span className={classes.event_info}>
-        { event.startTime.format('HH:mm') } - { event.endTime.format('HH:mm') }
+        {event.startTime.format('HH:mm')} - {event.endTime.format('HH:mm')}
       </span>
-
     </div>
   );
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     deleteAppointment: deleteAppointment,
-    dispatch
+    dispatch,
   };
 };
 
