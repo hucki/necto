b64ae682c7fb8eaaea2488506bf664746033a9de
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-09-04 14:59:02 +0200

    chore(server): setup basic express server with typescript

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3001;
+
+const app: express.Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server running at http://${host}:${port} 🚀`)
+})
