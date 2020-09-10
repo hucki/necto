3753594a748d223f6a8b187acf7c8be0ff1b097d
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-09-10 11:34:37 +0200

    fix(sequelize): update config

--- server/src/db/config/config.ts
+++ server/src/db/config/config.ts
@@ -1,11 +1,13 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const config = {
+const config = {
   development: {
     username: process.env.DEV_DB_USER,
     password: process.env.DEV_DB_PASSWORD,
     database: process.env.DEV_DB_NAME,
     host: process.env.DEV_DB_HOST,
     dialect: 'postgres',
-  }
+  },
 };
+export default config;
