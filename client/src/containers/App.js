dd69461995a8ad4e7d9e25fcb2ef1a271fb31db6
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-09-10 20:23:27 +0200

    chore(client): add react-query dev-tools to App

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -7,24 +7,28 @@ import { createStore } from 'redux';
 import reducer from '../reducers/index';
 import { Provider } from 'react-redux';
 import AppMenu from '../components/AppMenu/AppMenu';
+import { ReactQueryDevtools } from 'react-query-devtools';
 
 const { Content: AntContent } = Layout;
 
 const store = createStore(reducer);
-store.subscribe(() => {/*console.log('NEW STATE', store.getState())*/});
-function App () {
+store.subscribe(() => {
+  /*console.log('NEW STATE', store.getState())*/
+});
+function App() {
   return (
-    <Provider store = { store }>
+    <Provider store={store}>
       <Layout style={{ minHeight: '100vh' }}>
         <AppMenu />
         <Layout className="site-layout">
           <Header />
-          <AntContent style={{ margin: '0 16px'}}>
-            <Dashboard style={{ height: '100%' }}/>
+          <AntContent style={{ margin: '0 16px' }}>
+            <Dashboard style={{ height: '100%' }} />
           </AntContent>
           <Footer />
         </Layout>
       </Layout>
+      <ReactQueryDevtools initialIsOpen />
     </Provider>
   );
 }
