8d0eb2655d5cd9047cb6f89156b26534be0042f9
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-09-11 10:43:22 +0200

    chore(db): add columns and keys to sql migration file

--- server/src/db/migrations/necto.sql
+++ server/src/db/migrations/necto.sql
@@ -1,12 +1,12 @@
 CREATE TABLE "users" ("id" SERIAL PRIMARY KEY, "fullName" varchar, "firstName" varchar, "lastName" varchar, "validUntil" timestamp, "createdAt" timestamp, "updatedAt" timestamp);
 
-CREATE TABLE "contracts" ("userId" int UNIQUE NOT NULL, "hoursPerWeek" int, "appointmentsPerWeek" int);
+CREATE TABLE "contracts" ("id" int UNIQUE NOT NULL, "userId" int, "hoursPerWeek" int, "appointmentsPerWeek" int, "validUntil" timestamp, "createdAt" timestamp, "updatedAt" timestamp);
 
-CREATE TABLE "userSettings" ("userId" int UNIQUE NOT NULL, "bgColor" varchar);
+CREATE TABLE "userSettings" ("id" int UNIQUE NOT NULL, "userId" int, "bgColor" varchar, "validUntil" timestamp, "createdAt" timestamp, "updatedAt" timestamp);
 
 CREATE TABLE "settings" ("calDays" varchar, "calHoursStart" int, "calHoursEnd" int, "timescaleWidth" int);
 
-CREATE TABLE "events" ("id" int, "userId" int, "type" varchar, "name" varchar, "startTime" timestamp, "endTime" timestamp, "rrule" varchar, "homeVisit" boolean);
+CREATE TABLE "events" ("id" int UNIQUE NOT NULL, "userId" int, "type" varchar, "name" varchar, "startTime" timestamp, "endTime" timestamp, "rrule" varchar, "homeVisit" boolean, "createdAt" timestamp, "updatedAt" timestamp);
 
 ALTER TABLE "contracts" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
 
