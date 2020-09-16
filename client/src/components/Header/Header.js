5bfdcf2a97d445cf5462d47df6624946a0b320ec
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-09-16 19:23:10 +0200

    feature(ProgressBar): remove progressBar from Header and List of TeamMembers

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,15 +2,12 @@ import React from 'react';
 import classes from './Header.module.css';
 import NavBar from '../NavBar/NavBar';
 import UserInfo from '../UserInfo/UserInfo';
-import ProgressBar from '../ProgressBar/ProgressBar';
 
-
-function Header () {
+function Header() {
   return (
     <div className={classes.Header + ' box'}>
       <h1 className={classes.logo}>necto</h1>
       <UserInfo />
-      <ProgressBar />
       <NavBar />
     </div>
   );
--- client/src/components/Team/TeamContainer/TeamContainer.js
+++ client/src/components/Team/TeamContainer/TeamContainer.js
@@ -1,56 +1,27 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Table, Progress } from 'antd';
+import { Table } from 'antd';
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
 dayjs.extend(isBetween);
 
-const TeamContainer = ({ teamMembers, events, currentDate }) => {
+const TeamContainer = ({ teamMembers }) => {
   const dataSource = teamMembers.map((member) => {
-    let plannedAppointments = 0;
-    events[member.firstName].map((event) => {
-      if (
-        dayjs(event.startTime).isBetween(
-          dayjs(currentDate).startOf('week'),
-          dayjs(currentDate).endOf('week')
-        )
-      ) {
-        plannedAppointments++;
-      }
-      return event;
-    });
     return {
       key: member.id,
-      id: member.id,
+
       'First Name': member.firstName,
       'Last Name': member.lastName,
-      'Plan Color': member.color,
+      'Plan Color': member.bgColor,
       'Week Plan': member.appointmentsPerWeek,
-      'Planning Progress': parseInt(
-        (plannedAppointments / member.appointmentsPerWeek) * 100
-      ),
     };
   });
 
   const columns = Object.keys(dataSource[0]).map((key) => {
-    if (key === 'Planning Progress') {
-      return {
-        title: key,
-        dataIndex: key,
-        key: key,
-        render: (progress) => (
-          <>
-            <Progress percent={progress} size="small" />
-          </>
-        ),
-      };
-    } else {
-      return {
-        title: key,
-        dataIndex: key,
-        key: key,
-      };
-    }
+    return {
+      title: key,
+      dataIndex: key,
+      key: key,
+    };
   });
   return (
     <>
@@ -59,12 +30,4 @@ const TeamContainer = ({ teamMembers, events, currentDate }) => {
   );
 };
 
-const MapStateToProps = (state) => {
-  return {
-    events: state.appointments.events,
-    teamMembers: state.appointments.teamMembers,
-    currentDate: state.current.currentDate,
-  };
-};
-
-export default connect(MapStateToProps, null)(TeamContainer);
+export default TeamContainer;
