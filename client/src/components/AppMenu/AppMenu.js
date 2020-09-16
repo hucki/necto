427ac943d1be1663c6c2532e5eae8e1094e7f983
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-09-16 18:46:50 +0200

    refactor(AppMenu): autoformat by eslint

--- client/src/components/AppMenu/AppMenu.js
+++ client/src/components/AppMenu/AppMenu.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { DesktopOutlined, TeamOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import {
+  DesktopOutlined,
+  TeamOutlined,
+  SettingOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import { Menu, Layout } from 'antd';
 import classes from './AppMenu.module.css';
 import { connect } from 'react-redux';
@@ -7,44 +12,64 @@ import { switchView } from '../../actions/actions';
 
 const { Sider: AntSider } = Layout;
 
-const AppMenu = ({currentDate, currentView, user, dispatch}) => {
-
+const AppMenu = ({ currentDate, currentView, user, dispatch }) => {
   const onClickHandler = (e) => {
     dispatch(switchView(e.key));
   };
 
   return (
-    <AntSider
-      collapsible
-      theme='dark'
-      breakpoint='lg'
-      collapsedWidth='0'
-      >
+    <AntSider collapsible theme="dark" breakpoint="lg" collapsedWidth="0">
       <h1 className={classes.menu}>Menu</h1>
-      <Menu theme='dark' defaultSelectedKeys={[currentView]} mode="inline">
-        <Menu.Item key='Appointments' icon={<DesktopOutlined />} onClick={onClickHandler}> Appointments</Menu.Item>
-        {user !== 'Guest' && <Menu.Item key='Personal' icon={<UserOutlined />} onClick={onClickHandler}> Personal Calendar </Menu.Item>}
-        <Menu.Item key='Team' icon={<TeamOutlined />} onClick={onClickHandler}> Team Member</Menu.Item>
-        <Menu.Item disabled key='Settings' icon={<SettingOutlined />} onClick={onClickHandler}> Settings </Menu.Item>
+      <Menu theme="dark" defaultSelectedKeys={[currentView]} mode="inline">
+        <Menu.Item
+          key="Appointments"
+          icon={<DesktopOutlined />}
+          onClick={onClickHandler}
+        >
+          {' '}
+          Appointments
+        </Menu.Item>
+        {user !== 'Guest' && (
+          <Menu.Item
+            key="Personal"
+            icon={<UserOutlined />}
+            onClick={onClickHandler}
+          >
+            {' '}
+            Personal Calendar{' '}
+          </Menu.Item>
+        )}
+        <Menu.Item key="Team" icon={<TeamOutlined />} onClick={onClickHandler}>
+          {' '}
+          Team Member
+        </Menu.Item>
+        <Menu.Item
+          disabled
+          key="Settings"
+          icon={<SettingOutlined />}
+          onClick={onClickHandler}
+        >
+          {' '}
+          Settings{' '}
+        </Menu.Item>
       </Menu>
     </AntSider>
-  )
-}
+  );
+};
 
-const MapStateToProps = state => {
+const MapStateToProps = (state) => {
   return {
     currentDate: state.current.currentDate,
     currentView: state.settings.currentView,
-    user: state.userData.currentUser
+    user: state.userData.currentUser,
   };
 };
 
-const MapDispatchToProps = dispatch => {
+const MapDispatchToProps = (dispatch) => {
   return {
     switchView,
-    dispatch
+    dispatch,
   };
 };
-
 
 export default connect(MapStateToProps, MapDispatchToProps)(AppMenu);
