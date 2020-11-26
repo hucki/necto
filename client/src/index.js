adaf9f5c5f0d0d0062d45138d4964d4bc76cac63
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-11-26 09:44:27 +0100

    feature(client): add AuthProvider to App

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
 import App from './containers/App';
+import { Auth0Provider } from "@auth0/auth0-react";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="necto.eu.auth0.com"
+      clientId="xYfQs4MCa6xPRb1xOdZ7LoU7QKy2cYQ2"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
