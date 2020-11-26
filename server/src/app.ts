174414d88e2df9f0e33ac9605c64d8714be44a79
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-11-26 18:38:19 +0100

    refactor(server) remove openid-connect code

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,17 +3,8 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
 import router from './routes';
-import { auth } from 'express-openid-connect';
 dotenv.config();
 import db from './db/models';
-const authConfig = {
-  authRequired: false,
-  auth0Logout: true,
-  baseURL: 'http://localhost:3000',
-  secret: process.env.AUTH0_SECRET,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
-};
 
 db.sequelize.sync(); //{force: true}
 const host = process.env.HOST || 'localhost';
@@ -24,7 +15,6 @@ const app: express.Application = express();
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
-app.use(auth(authConfig))
 app.use('/api', router);
 
 app.listen(port, () => {
