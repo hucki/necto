6cf060fb96b156c09a7d5a159dff549ff6ffc356
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-11-26 15:31:23 +0100

    feature(client): add backennd user retrieval

--- client/src/containers/AuthenticatedApp.tsx
+++ client/src/containers/AuthenticatedApp.tsx
@@ -5,11 +5,13 @@ import Header from '../components/Header/Header';
 import Dashboard from '../components/Dashboard/Dashboard';
 import Footer from '../components/Footer/Footer';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0User } from '../hooks/user';
 
 const { Content: AntContent } = Layout;
 
 function AuthenticatedApp(): JSX.Element {
-  const { user } = useAuth0();
+  const { user: auth0User } = useAuth0();
+  const { user } = useAuth0User(auth0User.sub);
   console.log(user);
   return (
     <>
