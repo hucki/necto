2ee6986a61275f50a7d15e18b82dd9942130d543
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-11-27 13:09:33 +0100

    feature(client): enhance userData reducer to change Name and Id

--- client/src/reducers/userData.js
+++ client/src/reducers/userData.js
@@ -6,10 +6,12 @@ export default function userData(state = initialState.userData, {type, payload})
   const newState = {};
   switch(type) {
     case LOG_IN:
-      newState.currentUser = payload;
+      newState.currentUser = payload.firstName;
+      newState.currentUserId = payload.id;
       return newState;
     case LOG_OUT:
       newState.currentUser = 'Guest';
+      newState.currentUserId = null;
       return newState;
     default:
       return state;
