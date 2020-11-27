9e40f84dd3018db20729786deb9973b850712bfc
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-11-27 13:55:00 +0100

    feature(client): add AppState typings

---
+++ client/src/types/AppState.ts
@@ -0,0 +1,50 @@
+import { Dayjs } from "dayjs";
+
+interface TeamMembers {
+  id: number;
+  firstName: string;
+  lastName: string;
+  appointmentsPerWeek: number;
+  color: string;
+  planningProcess: number;
+}
+
+export type AppState = {
+    appointments: {
+      teamMembers: TeamMembers;
+    };
+    current: {
+      currentDate: Dayjs;
+    };
+    settings: {
+      hoursInterval: [number, number];
+      currentView: string;
+    };
+    newAppointment: {
+      inputFormVisible: boolean;
+      clickedRowId: string;
+      startTime: Dayjs;
+      endTime: Dayjs;
+      rrule: string;
+    };
+    userData: {
+      currentUser: string;
+      currentUserId: number;
+    };
+    teamtable: {
+      viewportDimensions: {
+        width: number;
+        height: number;
+        top: number;
+      };
+      calculatedDimensions: {
+        cellHeight: number;
+        cellWidth: number;
+        relCellHeight: number;
+      };
+      settings: {
+        daysToShow: string[];
+        timeScaleWidth: number;
+      };
+    };
+  };
