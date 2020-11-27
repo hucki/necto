e7582a64f128a80d1d7f46224795b6094847eda3
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-11-27 13:54:30 +0100

    refactor(client): refactor dashboard to typescript

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import classes from './Dashboard.module.css';
@@ -7,12 +7,17 @@ import TeamContainer from '../Team/TeamContainer/TeamContainer';
 import TimetableContainer from '../Timetable/TimetableContainer/TimetableContainer';
 import { useAllTeamMembers } from '../../hooks/user';
 import UserProfile from '../UserProfile/UserProfile';
+import { AppState } from '../../types/AppState';
 
-const Dashboard = ({a0Id}) => {
+interface DashboardInputProps {
+  a0Id: string,
+  style?: CSSProperties
+}
+const Dashboard = ({a0Id}: DashboardInputProps):JSX.Element => {
   const { isLoading, error, teamMembers } = useAllTeamMembers();
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error getting teamMembers: {error.message}</div>;
-  if (!teamMembers) return null;
+  if (error) return <div>Error getting teamMembers: {error}</div>;
+  if (!teamMembers) return <div>Data missing</div>;
 
 
   return (
@@ -41,7 +46,7 @@ const Dashboard = ({a0Id}) => {
     </div>);
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state: AppState) => {
   return {
     currentView: state.settings.currentView,
   };
