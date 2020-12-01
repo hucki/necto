c05eb34ba51f5216a8b19df934ea1ecc35168a1b
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-12-01 14:42:31 +0100

    feature(client): add LoadingSpinner if Authentication isLoading

--- client/src/containers/UnauthenticatedApp.tsx
+++ client/src/containers/UnauthenticatedApp.tsx
@@ -3,25 +3,32 @@ import { Layout } from 'antd';
 import LoginButton from '../components/Auth/LoginButton';
 import { Route, Switch } from 'react-router';
 import VerifySignup from '../components/Auth/VerifySignup';
+import { useAuth0 } from '@auth0/auth0-react';
+import Header from '../components/Header/Header';
+import { FullPageSpinner } from '../components/Library';
 
 
 function UnauthenticatedApp(): JSX.Element {
-
+  const { isLoading } = useAuth0();
   return (
-    <>
+    <div>
       <Layout className="site-layout">
-        <Switch>
-          <Route exact path="/">
-            <h1>Welcome</h1>
-            <div>Please login</div>
-          <LoginButton />
-          </Route>
-          <Route path="/verify">
-            <VerifySignup />
-          </Route>
-        </Switch>
+        <Header userName="Guest" />
+        {isLoading
+          ? <FullPageSpinner />
+          : <Switch>
+              <Route path="/verify">
+                <VerifySignup />
+              </Route>
+              <Route path="*">
+                <h1>Welcome</h1>
+                <div>Please login</div>
+                <LoginButton />
+              </Route>
+            </Switch>
+        }
       </Layout>
-    </>
+    </div>
   );
 }
 
