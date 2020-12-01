13b13f32e0dd5598e883d7ba194617dcda42425c
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-12-01 14:32:29 +0100

    feature(client): remove auth0 state logging

--- client/src/components/Auth/VerifySignup.tsx
+++ client/src/components/Auth/VerifySignup.tsx
@@ -1,15 +1,8 @@
 import React from "react";
-import { Link, Redirect, useLocation } from "react-router-dom";
-import {useAuth0} from '@auth0/auth0-react'
+import { Link } from "react-router-dom";
 import LogoutButton from "./LogoutButton";
 
 const VerifySignup = ():JSX.Element => {
-  const { isAuthenticated } = useAuth0();
-  const search = useLocation().search;
-  const auth0State = new URLSearchParams(search).get('state');
-  console.log(auth0State)
-
-  if(isAuthenticated) return <Redirect to="/" />
 
   return (
     <>
