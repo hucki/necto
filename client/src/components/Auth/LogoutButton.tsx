420581b1e2f70d4cefd05859d04c72750ebe9178
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-12-01 19:12:57 +0100

    feature(client): use Button from component Library

--- client/src/components/Auth/LogoutButton.tsx
+++ client/src/components/Auth/LogoutButton.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "../Library";
 
 const LogoutButton = ():JSX.Element => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <Button onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
-    </button>
+    </Button>
   );
 };
 
