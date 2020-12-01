bae075b830cf3ea129ace25d7bce76d3b29c681b
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-12-01 19:13:05 +0100

    feature(client): use Button from component Library

--- client/src/components/Auth/LoginButton.tsx
+++ client/src/components/Auth/LoginButton.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "../Library";
 
 const LoginButton = ():JSX.Element => {
   const { loginWithRedirect } = useAuth0();
-
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
 };
 
 export default LoginButton;
