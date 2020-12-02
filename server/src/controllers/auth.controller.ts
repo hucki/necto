d563eb08e3dc863e4583d90d31353e70e0256d8d
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-12-02 20:54:40 +0100

    refactor(server): remove unused openIdConnect controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,32 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { AppSettings } from '../db/models/AppSettings';
 import dotenv from 'dotenv';
-import { OpenidRequest, OpenidResponse } from 'express-openid-connect';
 dotenv.config();
-
-/**
- * add App Settings
- *  @param {AppSettings} req.body
- */
-export const isAuthenticated = (
-  req: OpenidRequest,
-  res: OpenidResponse,
-  next: NextFunction
-): void => {
-  try {
-    if(req.oidc.isAuthenticated()) {
-      res.status(200);
-      res.send();
-      return;
-    } else {
-      res.status(401);
-      res.send();
-      return;
-    }
-  } catch(e) {
-    next(e)
-  }
-}
 
 export const signupUser = async (
   req: Request,
