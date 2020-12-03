ee9f8aee3e189c7d5e9af30b9839d5007606e73d
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-12-03 15:48:24 +0100

    feature(client): add ressources and hoursRange calculation

--- client/src/components/Calendar/CalendarContainer.tsx
+++ client/src/components/Calendar/CalendarContainer.tsx
@@ -1,23 +1,53 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
-import dayjs from 'dayjs';
+import { useState } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
 import { Event } from '../../types/Event';
+import { AppState } from '../../types/AppState';
+import { Ressource } from '../../types/Ressource';
+import { connect } from 'react-redux';
 
 interface CalendarInputProps {
-  events: Event[]
+  events: Event[];
+  ressources: Ressource[];
+  hoursInterval?: [number, number];
+  currentDate?: Dayjs;
+  daysRange: [Dayjs, Dayjs];
 }
-function CalendarContainer({events}: CalendarInputProps):JSX.Element {
+const currentDayjs = dayjs();
+
+function CalendarContainer({events, hoursInterval, ressources, currentDate, daysRange = [currentDayjs, currentDayjs]}: CalendarInputProps):JSX.Element {
+  const [ currentHoursInterval, setCurrentHoursInterval ] = useState(hoursInterval);
   const days : string[] = [];
   events.map(event => {
     return dayjs(event.startTime).format('YYYY-MM-DD')
   })
   const dayRows = days.map(day => <div>day</div>)
+  const ressourceRows = ressources.map(ressource => <div id={`${ressource.id}`}>{ressource.shortDescription}</div>)
+  console.log(ressourceRows)
+  // calculate boundaries to fit all events
+  events.forEach(event => {
+    const newStart = dayjs(event.startTime).hour();
+    const newEnd = dayjs(event.endTime).hour() + 1;
+    console.log([newStart, newEnd])
+    if (currentHoursInterval === undefined) {
+      setCurrentHoursInterval([newStart, newEnd]);
+      return;
+    }
+    if (currentHoursInterval && newStart < currentHoursInterval[0] ) {
+      setCurrentHoursInterval(current => [newStart, current === undefined ? newEnd : current[1]]);
+    }
+    if (currentHoursInterval && newEnd > currentHoursInterval[1] ) {
+      setCurrentHoursInterval(current => [current === undefined ? newStart : current[0], newEnd]);
+    }
+  })
+  if (currentHoursInterval === undefined) return <div>no hoursInterval</div>
   return (
-    <div
+    <div id="containerDiv"
       css={{
-        fontSize: '4em',
-        height: '100vh',
-        width: '100vw',
+        fontSize: '1rem',
+        height: '100%',
+        width: '100%',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
@@ -25,13 +55,19 @@ function CalendarContainer({events}: CalendarInputProps):JSX.Element {
         textAlign: 'center'
       }}
     >
-      <div>CalendarScale from appSettings</div>
+      <div id="CalendarScale">CalendarScale from {currentHoursInterval[0]} to {currentHoursInterval[1]} </div>
       {dayRows}
-      <div>one CalendarDay per Date in Events Object
-        <div>one CalendarRow per Ressource/Person</div>
+      <div id="CalendarDay">one CalendarDay per Date in Events Object
+      <div id="CalendarRow">one CalendarRow per Ressource/Person: {ressourceRows}</div>
       </div>
     </div>
   )
 }
+const mapStateToProps = (state: AppState) => {
+  return {
+    currentDate: state.current.currentDate,
+    hoursInterval: state.settings.hoursInterval,
+  };
+};
 
-export {CalendarContainer};
+export default connect(mapStateToProps, null)(CalendarContainer);
