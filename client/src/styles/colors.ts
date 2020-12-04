6969c3701d411f4047bd7fabbedc6faafe0c024f
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-12-04 15:24:33 +0100

    feature(client): add background colors to library

--- client/src/styles/colors.ts
+++ client/src/styles/colors.ts
@@ -14,3 +14,19 @@ export const yellow = '#ffc107'
 export const green = '#4caf50'
 export const danger = '#ef5350'
 export const orange = 'orange'
+export const bg = {
+  blue50: '#EFF6FF',
+  blue500: '#3B82F6',
+  red50: '#FEF2F2',
+  red500: '#EF4444',
+  green50: '#F0FDF4',
+  green500: '#22C55E',
+  yellow50: '#FEFCE8',
+  yellow500: '#EAB308',
+  grey50: '#FAFAFA',
+  grey500: '#71717A',
+  cyan50: '#ECFEFF',
+  cyan500:  '#06B6D4',
+  magenta50: '#FDF4FF',
+  magenta500: '#D946EF'
+}
