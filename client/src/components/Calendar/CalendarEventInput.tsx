9fc46f8c05a4bada9963d1fbb6793a615e79fb1f
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-12-06 20:40:48 +0100

    feature(client): add style to modal component

--- client/src/components/Calendar/CalendarEventInput.tsx
+++ client/src/components/Calendar/CalendarEventInput.tsx
@@ -7,9 +7,10 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton,
-  Button,
-} from "@chakra-ui/react"
+  ModalCloseButton
+} from '@chakra-ui/react';
+import { Button, CircleButton } from '../Library';
+import * as mq from '../../styles/media-queries';
 import { Dayjs } from 'dayjs';
 
 interface CalendarEventInputProps {
@@ -21,25 +22,42 @@ interface CalendarEventInputProps {
 }
 
 function CalendarEventInput({id, dateTime, isOpen, onOpen, onClose}: CalendarEventInputProps): JSX.Element {
-
+  console.log(id, dateTime.format('YYYYMMDD hh:mm'))
   return (
     <div>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>New Event</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            {id + dateTime.format('YYYYMMDD hh:mm')}
-          </ModalBody>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay>
+          <ModalContent
+            css={{
+              maxWidth: '450px',
+              borderRadius: '3px',
+              paddingBottom: '3.5em',
+              backgroundColor: 'white',
+              boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
+              margin: '20vh auto',
+              [mq.small]: {
+                width: '100%',
+                margin: '10vh auto',
+              }
+            }}
+          >
+            <ModalHeader>New Appointment {dateTime.format('YYYYMMDD')}</ModalHeader>
+            <CircleButton />
+            {/* <ModalCloseButton /> */}
+            <ModalBody>
+              New Appointment for User: {id} starting {dateTime.format('YYYYMMDD hh:mm')}
+            </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
+            <ModalFooter>
+              <Button onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
       </Modal>
     </div>
   )
