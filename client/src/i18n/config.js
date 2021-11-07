8df879fabc98d8edf74a2368b5db9fc68856cf33
Stefan Huckschlag <stefan.huckschlag@web.de>
2021-11-07 22:42:30 +0100

    feature(i18next_config): use LanguageDetector

--- client/src/i18n/config.js
+++ client/src/i18n/config.js
@@ -1,20 +1,24 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n.use(initReactI18next).init({
-  fallbackLng: 'en',
-  lng: 'de',
-  resources: {
-    de: {
-      translations: require('./locales/de/translations.json')
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    resources: {
+      de: {
+        translations: require('./locales/de/translations.json'),
+      },
+      en: {
+        translations: require('./locales/en/translations.json'),
+      },
     },
-    en: {
-      translations: require('./locales/en/translations.json')
-    }
-  },
-  ns: ['translations'],
-  defaultNS: 'translations'
-});
+    ns: ['translations'],
+    defaultNS: 'translations',
+  });
 
 i18n.languages = ['de', 'en'];
 
