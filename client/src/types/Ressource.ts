fb5c378453c3111dbc9959ede5dde087eaa90127
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-01-15 20:55:52 +0100

    feature(types_ressources): add displayName, color, building to type

--- client/src/types/Ressource.ts
+++ client/src/types/Ressource.ts
@@ -1,6 +1,10 @@
+import { BgColor } from './Colors';
+
 export type Ressource = {
   id: number;
+  displayName: string;
   shortDescription: string;
   longDescription: string;
-  bgColor: string;
+  bgColor: BgColor;
+  building?: number;
 };
