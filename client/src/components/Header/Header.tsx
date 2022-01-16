33327200693c1a3407263e5b71bbaead01dfb99c
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-01-16 22:28:30 +0100

    refactor(Header): refactor to TS

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,11 +3,15 @@ import classes from './Header.module.css';
 import NavBar from '../NavBar/NavBar';
 import UserInfo from '../UserInfo/UserInfo';
 
-function Header ({userName}) {
+interface HeaderProps {
+  userName: string;
+}
+
+function Header({ userName }: HeaderProps) {
   return (
     <div className={classes.Header + ' box'}>
       <h1 className={classes.logo}>necto</h1>
-      <UserInfo userName={userName}/>
+      <UserInfo userName={userName} />
       <NavBar />
     </div>
   );
