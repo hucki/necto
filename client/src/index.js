4c4c0a4521510c0fff5aadeabbe411fe00c3c89a
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-01-18 08:53:44 +0100

    fix(index): import min version of antd to prevent webpack errors. see antd issue 33327

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import App from './containers/App';
 import './i18n/config';
 import { Auth0Provider } from '@auth0/auth0-react';
