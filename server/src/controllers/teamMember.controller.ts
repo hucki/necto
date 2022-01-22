f199fdcd4b367e67ce05f5a3bd1aac5798d3de2e
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-01-23 00:27:18 +0100

    feat(teamMemberController): switch to employees

--- server/src/controllers/teamMember.controller.ts
+++ server/src/controllers/teamMember.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
+import prisma from '../db/prisma';
 import dotenv from 'dotenv';
 dotenv.config();
 const tenantId = process.env.TENANT_UUID;
-import db from '../db/models';
+// import db from '../db/models';
 
 export const getAllTeamMembers = async (
   req: Request,
@@ -10,27 +11,28 @@ export const getAllTeamMembers = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    const [results, metadata] = await db.sequelize.query(
-      `SELECT
-          users.id AS id,
-          users."firstName" AS "firstName",
-          users."lastName" AS "lastName",
-          users."validUntil" AS "validUntil",
-          users."createdAt" AS "createdAt",
-          users."updatedAt" AS "updatedAt",
-          contracts."hoursPerWeek" AS "hoursPerWeek",
-          contracts."appointmentsPerWeek" AS "appointmentsPerWeek",
-          "userSettings"."bgColor" AS "bgColor"
-      FROM users
-      LEFT JOIN contracts
-        ON users.id=contracts."userId"
-        AND users."tenantId"=contracts."tenantId"
-      LEFT JOIN "userSettings"
-        ON users.id="userSettings"."userId"
-        AND users."tenantId"="userSettings"."tenantId"
-      WHERE users."tenantId" = '${tenantId}'`
-    );
-    res.json(results);
+    const teamMembers = await prisma.employee.findMany();
+    // const [results, metadata] = await db.sequelize.query(
+    //   `SELECT
+    //       users.id AS id,
+    //       users."firstName" AS "firstName",
+    //       users."lastName" AS "lastName",
+    //       users."validUntil" AS "validUntil",
+    //       users."createdAt" AS "createdAt",
+    //       users."updatedAt" AS "updatedAt",
+    //       contracts."hoursPerWeek" AS "hoursPerWeek",
+    //       contracts."appointmentsPerWeek" AS "appointmentsPerWeek",
+    //       "userSettings"."bgColor" AS "bgColor"
+    //   FROM users
+    //   LEFT JOIN contracts
+    //     ON users.id=contracts."userId"
+    //     AND users."tenantId"=contracts."tenantId"
+    //   LEFT JOIN "userSettings"
+    //     ON users.id="userSettings"."userId"
+    //     AND users."tenantId"="userSettings"."tenantId"
+    //   WHERE users."tenantId" = '${tenantId}'`
+    // );
+    res.json(teamMembers);
     res.status(200);
     return;
   } catch (e) {
