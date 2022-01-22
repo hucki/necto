98726c243d33a41e74c64733f01026c4946461f2
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-01-23 00:28:30 +0100

    feat(server_app): remove sequelize

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,16 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
-import {router, errorRouter } from './routes';
+import { router } from './routes';
 dotenv.config();
-import db from './db/models';
-let db_error = null;
-db.sequelize
-  .sync(/*{force: true}*/)
-  .catch(error => {
-    db_error = error.message;
-    console.log(`⚠️ Error connecting to DB`, error.message);
-  })
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3001;
 
@@ -20,10 +12,7 @@ const app: express.Application = express();
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
-app.use('/api', db_error
-  ? errorRouter
-  : router);
-
+app.use('/api', router);
 app.listen(port, () => {
   console.log(`Server running at http://${host}:${port} 🚀`);
 });
