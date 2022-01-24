16d6bb3a1f4a777952cf29454882a1a407cd2455
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-01-24 21:31:16 +0100

    feat(NavBar): move currentTeam filter into FilterBar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,40 +1,19 @@
-import React, { Dispatch, useEffect, useState } from 'react';
+import React, { Dispatch } from 'react';
 import { connect } from 'react-redux';
 import { Button, Tooltip } from 'antd';
 import { CaretRightOutlined, CaretLeftOutlined } from '@ant-design/icons';
 import { DatePicker } from '../../elements';
-import { changeDate, setCurrentTeam } from '../../actions/actions';
+import { changeDate } from '../../actions/actions';
 import { AppState } from '../../types/AppState';
 import classes from './NavBar.module.css';
 import dayjs, { Dayjs } from 'dayjs';
-import { useAllTeams } from '../../hooks/teams';
-import { Team } from '../../types/Employee';
 
 interface NavBarProps {
   currentDate: Dayjs;
-  // currentTeam: Team;
   dispatch: Dispatch<any>;
 }
 
-const NavBar = ({ currentDate, /*currentTeam,*/ dispatch }: NavBarProps) => {
-  const { isLoading: isLoadingTeams, error, teams } = useAllTeams();
-  const [currentTeam, setCurrentTeam] = useState<Team>();
-
-  useEffect(() => {
-    if (!isLoadingTeams && teams.length) {
-      setCurrentTeam(teams[0]);
-    }
-  }, [isLoadingTeams]);
-  const onTeamChangeHandler = (event: any) => {
-    setCurrentTeam(teams.filter((t) => t.uuid === event.target.value)[0]);
-  };
-
-  // function onTeamChangeHandler(event: any) {
-  //   dispatch(
-  //     setCurrentTeam(teams.filter((t) => t.uuid === event.target.value)[0])
-  //   );
-  // }
-
+const NavBar = ({ currentDate, dispatch }: NavBarProps) => {
   function onChangeHandler(date: Dayjs | null) {
     dispatch(changeDate(date));
   }
@@ -50,19 +29,6 @@ const NavBar = ({ currentDate, /*currentTeam,*/ dispatch }: NavBarProps) => {
 
   return (
     <div className={classes.NavBar}>
-      {currentTeam && (
-        <select
-          name="team"
-          value={currentTeam.uuid}
-          onChange={onTeamChangeHandler}
-        >
-          {teams.map((t, i) => (
-            <option key={i} value={t.uuid}>
-              {t.displayName}
-            </option>
-          ))}
-        </select>
-      )}
       <Button
         disabled={dayjs().isSame(dayjs(currentDate), 'day')}
         onClick={todayClickHandler}
@@ -81,13 +47,12 @@ const NavBar = ({ currentDate, /*currentTeam,*/ dispatch }: NavBarProps) => {
 };
 
 const MapStateToProps = (state: AppState) => {
-  const { currentDate /*, currentTeam */ } = state.current;
-  return { currentDate /*, currentTeam */ };
+  const { currentDate } = state.current;
+  return { currentDate };
 };
 const MapDispatchToProps = (dispatch: Dispatch<any>) => {
   return {
     changeDate,
-    setCurrentTeam,
     dispatch,
   };
 };
