5c4a872f45feadf765cbbd751db460bae15b0dd3
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-02-25 13:11:28 +0100

    feat(patient_hook): add useCreatePatient

--- client/src/hooks/patient.ts
+++ client/src/hooks/patient.ts
@@ -1,6 +1,33 @@
-import { useQuery, QueryResult } from 'react-query';
+import {
+  useQuery,
+  QueryResult,
+  useMutation,
+  MutationResultPair,
+  queryCache,
+} from 'react-query';
 import { useAuthenticatedClient } from '../services/ApiClient';
 import { Patient } from '../types/Patient';
+
+export function useCreatePatient(): MutationResultPair<
+  Patient,
+  Error,
+  { patient: Patient },
+  string
+  > {
+  const client = useAuthenticatedClient<Patient>();
+  const createPatient = async ({
+    patient,
+  }: {
+    patient: Patient;
+  }): Promise<Patient> => {
+    return client('patients', { data: patient });
+  };
+  return useMutation(createPatient, {
+    onSuccess: () => {
+      queryCache.invalidateQueries('patients');
+    },
+  });
+}
 
 export function useAllPatients(): QueryResult<Patient[]> & {
   patients: Patient[];
