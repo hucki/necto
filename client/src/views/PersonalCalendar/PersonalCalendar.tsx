aa6d2bfab1da7ae5543ec9300fc6b6934af13b11
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-04-12 00:01:36 +0200

    feat(PersonalCalendar): add week view

--- client/src/views/PersonalCalendar/PersonalCalendar.tsx
+++ client/src/views/PersonalCalendar/PersonalCalendar.tsx
@@ -6,41 +6,46 @@ import CalendarContainer from '../../components/Calendar/CalendarContainer';
 import { AppState } from '../../types/AppState';
 import { useDaysEvents, useWeeksEvents } from '../../hooks/events';
 import dayjs, { Dayjs } from 'dayjs';
-import { FullPageSpinner } from '../../components/Library';
-import { useEffect, useState } from 'react';
-import { Employee, Team } from '../../types/Employee';
+import { FullPageSpinner, Label, Select } from '../../components/Library';
+import { useContext, useEffect, useState } from 'react';
 import { EmployeeRessource } from '../../types/Ressource';
 import { useAuth0User } from '../../hooks/user';
 import weekOfYear from 'dayjs/plugin/weekOfYear';
-dayjs.extend(weekOfYear);
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+import utc from 'dayjs/plugin/utc';
+import { Flex } from '@chakra-ui/react';
+import { UserDateContext } from '../../providers/UserDate';
 
-interface TeamCalendarInputProps {
-  currentDate?: Dayjs;
-  currentTeam?: Team;
+dayjs.extend(weekOfYear);
+dayjs.extend(LocalizedFormat);
+dayjs.extend(utc);
+dayjs.locale('de');
+
+interface PersonalCalendarInputProps {
   a0Id: string;
 }
 
 function PersonalCalendar({
-  currentDate,
-  currentTeam,
   a0Id,
-}: TeamCalendarInputProps): JSX.Element {
+}: PersonalCalendarInputProps): JSX.Element {
+  const {currentDate} = useContext(UserDateContext);
   const [calendarDate, setCalendarDate] = useState(
     currentDate ? currentDate : dayjs()
   );
-  // const { isLoading, rawEvents } = useDaysEvents(calendarDate);
-  const { isLoading, rawEvents } = useWeeksEvents(calendarDate.year(), calendarDate.week()-1);
+  const { isLoading: isLoadingDaysEvents, rawEvents: daysEvents } = useDaysEvents(calendarDate);
+  const { isLoading: isLoadingWeeksEvents, rawEvents: weeksEvents } = useWeeksEvents(calendarDate.year(), calendarDate.week());
   const { user, isLoading: isLoadingUser } = useAuth0User(a0Id);
-  console.log('test');
+  const isLoading = isLoadingDaysEvents || isLoadingWeeksEvents || isLoadingUser;
+
+  const [currentView, setCurrentView] = useState<'day'|'week'>('day');
+  const onCurrentViewChange = (event: any) => {
+    setCurrentView(event.target.value);
+  };
+
   useEffect(() => {
     if (currentDate && calendarDate !== currentDate)
       setCalendarDate(currentDate);
   }, [currentDate, calendarDate, setCalendarDate]);
-  const teamMembers = currentTeam?.employees;
-
-  interface TeamMemberMapProps {
-    employee: Employee;
-  }
 
   if (isLoading || isLoadingUser || !user?.uuid || !user?.userSettings?.length || !user?.userSettings[0].employee) return <FullPageSpinner />;
 
@@ -67,20 +72,41 @@ function PersonalCalendar({
         alignItems: 'center',
       }}
     >
-      <CalendarContainer
-        readOnly={false}
-        events={rawEvents}
-        ressources={ressources}
-        daysRange={[calendarDate, calendarDate.add(6, 'day')]}
-      />
+      <Flex w={300}>
+        <Label htmlFor="view">View</Label>
+        <Select
+          name="view"
+          value={currentView}
+          onChange={onCurrentViewChange}
+        >
+          <option value="day">Day</option>
+          <option value="week">Week</option>
+        </Select>
+      </Flex>
+      {currentView === 'day' &&
+        <CalendarContainer
+          readOnly={false}
+          events={daysEvents}
+          ressources={ressources}
+          daysRange={[calendarDate, calendarDate]}
+          columnHeaderFormat={'dddd DD.MM.'}
+        />
+      }
+      {currentView === 'week' &&
+        <CalendarContainer
+          readOnly={false}
+          events={weeksEvents}
+          ressources={ressources}
+          daysRange={[calendarDate.startOf('week'), calendarDate.startOf('week').add(6, 'day')]}
+          columnHeaderFormat={'DD.MM.'}
+        />
+      }
     </div>
   );
 }
 
 const mapStateToProps = (state: AppState) => {
   return {
-    currentTeam: state.currentTeam,
-    currentDate: state.current.currentDate,
     hoursInterval: state.settings.hoursInterval,
   };
 };
