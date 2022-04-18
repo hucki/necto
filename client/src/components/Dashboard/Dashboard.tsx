dad4918859a4b242a8a11477150f9d37471966f8
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-04-19 00:13:13 +0200

    refactor(Dashboard): remove unused code

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -3,17 +3,13 @@ import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import classes from './Dashboard.module.css';
 import { useAllTeamMembers } from '../../hooks/user';
-import UserProfile from '../UserProfile/UserProfile';
 import { AppState } from '../../types/AppState';
 import TeamCalendar from '../../views/TeamCalendar/TeamCalendar';
 import PersonalCalendar from '../../views/PersonalCalendar/PersonalCalendar';
 import Rooms from '../../views/Rooms/Rooms';
-import TeamSettings from '../../views/TeamSettings/TeamSettings';
-import EmployeeSettings from '../../views/EmployeeSettings/EmployeeSettings';
 import Patients from '../../views/Patients/Patients';
 import WaitingList from '../../views/Patients/WaitingList';
 import Settings from '../../views/Settings/Settings';
-// import { useBreakpointValue } from '@chakra-ui/react';
 
 interface DashboardInputProps {
   a0Id: string;
@@ -24,7 +20,6 @@ const Dashboard = ({ a0Id }: DashboardInputProps): JSX.Element => {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error getting teamMembers: {error}</div>;
   if (!teamMembers) return <div>Data missing</div>;
-  // const viewport = useBreakpointValue({ base: 'mobile', md: 'desktop' });
 
   return (
     <div className={classes.Dashboard}>
