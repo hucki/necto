bed7b02cdbc6502c51ae233a0c891c84f549e439
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-04-19 00:19:06 +0200

    feat(PatientsWaiting): remove FilterBar

--- client/src/views/Patients/Patients.tsx
+++ client/src/views/Patients/Patients.tsx
@@ -3,7 +3,6 @@
 import { Spinner } from '@chakra-ui/react';
 import { jsx } from '@emotion/react';
 import { connect } from 'react-redux';
-import FilterBar from '../../components/FilterBar/FilterBar';
 import PatientsList from '../../components/Patients/PatientsList';
 import { useAllPatients } from '../../hooks/patient';
 
@@ -21,11 +20,10 @@ function Patients(): JSX.Element {
         width: '100%',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'flex-start',
       }}
     >
-      <FilterBar hasCompanyFilter />
       <PatientsList patients={patients} hasActions />
     </div>
   );
--- client/src/views/Patients/WaitingList.tsx
+++ client/src/views/Patients/WaitingList.tsx
@@ -3,7 +3,6 @@
 import { Spinner } from '@chakra-ui/react';
 import { jsx } from '@emotion/react';
 import { connect } from 'react-redux';
-import FilterBar from '../../components/FilterBar/FilterBar';
 import PatientsList from '../../components/Patients/PatientsList';
 import { useAllWaitingPatients } from '../../hooks/patient';
 
@@ -21,15 +20,11 @@ function WaitingList(): JSX.Element {
         width: '100%',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'flex-start',
       }}
     >
-      <FilterBar hasCompanyFilter />
-      <PatientsList
-        patients={patients}
-        hasActions={false}
-      />
+      <PatientsList patients={patients} />
     </div>
   );
 }
