dc0122e8ec5f5735474e153bf07892c285661aac
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-04-26 20:42:51 +0200

    feat(PatientForm): add second col for non automatic fields

--- client/src/components/Patients/PatientForm.tsx
+++ client/src/components/Patients/PatientForm.tsx
@@ -1,7 +1,8 @@
-import { Icon } from '@chakra-ui/react';
+import { GridItem, Icon, SimpleGrid } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { RiCheckboxBlankLine, RiCheckLine } from 'react-icons/ri';
+import { useViewport } from '../../hooks/useViewport';
 import { Patient } from '../../types/Patient';
 import { Input, Label, ModalFormGroup, TextDisplay } from '../Library';
 
@@ -13,7 +14,7 @@ interface PatientFormProps {
 
 type PatientKey = keyof Patient;
 
-const PatientKeysToShow: PatientKey[] = [
+const autoFormFieldKeys: PatientKey[] = [
   'firstName',
   'lastName',
   'gender',
@@ -28,6 +29,7 @@ const PatientKeysToShow: PatientKey[] = [
 ];
 
 export const PatientForm = ({ patient, type = 'view', onChange }: PatientFormProps) => {
+  const { isMobile } = useViewport();
   const { t } = useTranslation();
   const [currentPatient, setCurrentPatient] = useState<Patient>(() => ({...patient}));
 
@@ -45,41 +47,58 @@ export const PatientForm = ({ patient, type = 'view', onChange }: PatientFormPro
     onChange(currentPatient);
   }, [currentPatient]);
 
+  const autoFormFields = () => {
+    return Object.keys(currentPatient)
+      .filter(
+        (key) =>
+          autoFormFieldKeys.findIndex((element) => element === key) !== -1
+      )
+      .map((key) =>
+        typeof currentPatient[key as keyof Patient] === 'string' ||
+        typeof currentPatient[key as keyof Patient] === 'boolean' ? (
+            <ModalFormGroup key={key}>
+              <Label htmlFor={key}>{t(`patients.${key}`)}</Label>
+              {typeof currentPatient[key as PatientKey] === 'boolean' ? (
+                <Icon
+                  id={key}
+                  as={
+                    currentPatient[key as PatientKey]
+                      ? RiCheckLine
+                      : RiCheckboxBlankLine
+                  }
+                  w={5}
+                  h={5}
+                  color={currentPatient[key as PatientKey] ? 'indigo' : 'gray.400'}
+                />
+              ) :
+                type === 'view' ? (
+                  <TextDisplay id={key}>
+                    {currentPatient[key as PatientKey]?.toString()}&nbsp;
+                  </TextDisplay>
+                ) : (
+                  <Input onChange={(e) => onInputChange({event: e, key: key as PatientKey})} id={key} value={currentPatient[key as PatientKey]?.toString()}></Input>
+                )}
+            </ModalFormGroup>
+          ) : null
+      );
+  };
   return (
     <>
-      {Object.keys(currentPatient)
-        .filter(
-          (key) =>
-            PatientKeysToShow.findIndex((element) => element === key) !== -1
-        )
-        .map((key) =>
-          typeof currentPatient[key as keyof Patient] === 'string' ||
-          typeof currentPatient[key as keyof Patient] === 'boolean' ? (
-              <ModalFormGroup key={key}>
-                <Label htmlFor={key}>{t(`patients.${key}`)}</Label>
-                {typeof currentPatient[key as PatientKey] === 'boolean' ? (
-                  <Icon
-                    id={key}
-                    as={
-                      currentPatient[key as PatientKey]
-                        ? RiCheckLine
-                        : RiCheckboxBlankLine
-                    }
-                    w={5}
-                    h={5}
-                    color={currentPatient[key as PatientKey] ? 'indigo' : 'gray.400'}
-                  />
-                ) :
-                  type === 'view' ? (
-                    <TextDisplay id={key}>
-                      {currentPatient[key as PatientKey]?.toString()}&nbsp;
-                    </TextDisplay>
-                  ) : (
-                    <Input onChange={(e) => onInputChange({event: e, key: key as PatientKey})} id={key} value={currentPatient[key as PatientKey]?.toString()}></Input>
-                  )}
-              </ModalFormGroup>
-            ) : null
-        )}
+      <SimpleGrid columns={[1, null, 2]} gap={6}>
+        <GridItem>{autoFormFields()}</GridItem>
+        <GridItem>
+          <ModalFormGroup>
+            <Label htmlFor="doctorId">{t('patients.doctor')}</Label>
+            {type === 'view' ? (
+              <TextDisplay id="doctorId">
+                {currentPatient['doctorId']}
+              </TextDisplay>
+            ) : (
+              <Input id="doctorId" onChange={(e) => onInputChange({event: e, key: 'doctorId'})} value={currentPatient['doctorId']}></Input>
+            )}
+          </ModalFormGroup>
+        </GridItem>
+      </SimpleGrid>
       <b>Termine:</b>{currentPatient.events && currentPatient.events.map(event => <div key={event.uuid}>{event.startTime}</div>)}
     </>
   );
