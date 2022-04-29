1f0e7a24ba126d55e7b1b5b7d0abd49dc3da373a
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-04-29 12:50:34 +0200

    feat(displayNames): new helper

---
+++ client/src/helpers/displayNames.ts
@@ -0,0 +1,15 @@
+import { Doctor } from '../types/Doctor';
+import { Patient } from '../types/Patient';
+
+export const getDisplayName = (p: Patient | Doctor) => {
+  const title = p.title ? p.title + ' ' : '';
+  const firstNameInitial = p.firstName ? p.firstName.substring(0,1) + '. ' : '';
+  const lastName = p.lastName ? p.lastName : '';
+  if ('firstContactAt' in p) {
+    // Patient
+    return title + firstNameInitial + lastName;
+  } else {
+    // Doctor
+    return title + firstNameInitial + lastName;
+  }
+};
