81ce31cecf4b386e397e17d964952bfdffed1356
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-05-28 19:08:01 +0200

    feat(unauthenticatedApp): use new Auth provider

--- client/src/containers/UnauthenticatedApp.tsx
+++ client/src/containers/UnauthenticatedApp.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import LoginButton from '../components/Auth/LoginButton';
-import { Route, Switch } from 'react-router';
-import VerifySignup from '../components/Auth/VerifySignup';
-import { useAuth0 } from '@auth0/auth0-react';
 import {
   App,
   ContentContainer,
-  FullPageSpinner,
-  Header,
 } from '../components/Library';
-import UserInfo from '../components/UserInfo/UserInfo';
 import { Box, Stack } from '@chakra-ui/react';
+import Login from '../views/Auth/Login';
 
 function UnauthenticatedApp(): JSX.Element {
-  const { isLoading } = useAuth0();
   return (
     <App id="App">
       <ContentContainer alignItems="center" justifyContent="center">
@@ -23,21 +16,9 @@ function UnauthenticatedApp(): JSX.Element {
           p="8"
         >
           <Stack spacing="4">
-            <UserInfo userName="Guest" />
-            {isLoading ? (
-              <FullPageSpinner />
-            ) : (
-              <Switch>
-                <Route path="/verify">
-                  <VerifySignup />
-                </Route>
-                <Route path="*">
-                  <h1>Welcome</h1>
-                  <div>Please login</div>
-                  <LoginButton />
-                </Route>
-              </Switch>
-            )}
+            <h1>Welcome</h1>
+            <div>Please login</div>
+            <Login />
           </Stack>
         </Box>
       </ContentContainer>
