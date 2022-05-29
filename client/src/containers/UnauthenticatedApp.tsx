2d92b43ff69e0222169ba28ba725a8793f9c4ba9
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-05-30 00:00:22 +0200

    feat(UnauthenticatedApp): add Register

--- client/src/containers/UnauthenticatedApp.tsx
+++ client/src/containers/UnauthenticatedApp.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   App,
   ContentContainer,
 } from '../components/Library';
-import { Box, Stack } from '@chakra-ui/react';
+import { Box, Stack, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import Login from '../views/Auth/Login';
+import Register from '../views/Auth/Register';
 
 function UnauthenticatedApp(): JSX.Element {
+  const [ registrationActive, setRegistrationActive ] = useState(false);
   return (
     <App id="App">
       <ContentContainer alignItems="center" justifyContent="center">
@@ -17,8 +19,24 @@ function UnauthenticatedApp(): JSX.Element {
         >
           <Stack spacing="4">
             <h1>Welcome</h1>
-            <div>Please login</div>
-            <Login />
+            <Tabs>
+              <TabList>
+                <Tab>
+                  Login
+                </Tab>
+                <Tab>
+                  Register
+                </Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <Login />
+                </TabPanel>
+                <TabPanel>
+                  <Register />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
           </Stack>
         </Box>
       </ContentContainer>
