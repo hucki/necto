36efd2d69ed29f1e9f5ea1374fdb67a5bb1cdcdd
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-06-26 18:45:21 +0200

    feat(AppointmentList): add pagination

--- client/src/components/List/Appointments.tsx
+++ client/src/components/List/Appointments.tsx
@@ -1,9 +1,11 @@
-import { ChakraProps, Icon, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { ChakraProps, Flex, Icon, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import dayjs from 'dayjs';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { FaCaretLeft, FaCaretRight } from 'react-icons/fa';
 import { RiCheckboxBlankLine, RiCheckLine } from 'react-icons/ri';
 import { Event } from '../../types/Event';
+import { Button, IconButton } from '../Library';
 
 interface AppointmentListProps {
   events: Event[]
@@ -14,24 +16,36 @@ export const AppointmentList = ({events}: AppointmentListProps) => {
     color: 'gray'
   };
   const {t} = useTranslation();
-  const EventRows = (): JSX.Element[] => events.map(event =>
-    <Tr key={event.uuid} css={event.isCancelled ? cancelStyle : undefined}>
-      <Td>{dayjs(event.startTime).format('llll')}</Td>
-      <Td>{event.employee?.alias}</Td>
-      <Td>
-        <Icon
-          as={
-            event.isDiagnostic
-              ? RiCheckLine
-              : RiCheckboxBlankLine
-          }
-          w={5}
-          h={5}
-          color={event.isDiagnostic ? 'indigo' : 'gray.400'}
-        />
-      </Td>
-    </Tr>
-  );
+
+  // pagination controls
+  const rowsPerPage = 6;
+  const numOfPages = Math.ceil(events.length / rowsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const EventRows = (): JSX.Element[] => events
+    // pagination filter
+    .filter(
+      (p: Event, i) =>
+        i < currentPage * rowsPerPage && i >= (currentPage - 1) * rowsPerPage
+    )
+    .map(event =>
+      <Tr key={event.uuid} css={event.isCancelled ? cancelStyle : undefined}>
+        <Td>{dayjs(event.startTime).format('llll')}</Td>
+        <Td>{event.employee?.alias}</Td>
+        <Td>
+          <Icon
+            as={
+              event.isDiagnostic
+                ? RiCheckLine
+                : RiCheckboxBlankLine
+            }
+            w={5}
+            h={5}
+            color={event.isDiagnostic ? 'indigo' : 'gray.400'}
+          />
+        </Td>
+      </Tr>
+    );
   return <>
     <Table variant="striped" size="sm" colorScheme="blue">
       <Thead>
@@ -45,5 +59,30 @@ export const AppointmentList = ({events}: AppointmentListProps) => {
         {EventRows()}
       </Tbody>
     </Table>
+    {/* pagination controls START */}
+    <Flex m={2} alignSelf="flex-end">
+      <IconButton
+        aria-label="previous page"
+        leftIcon={<FaCaretLeft />}
+        disabled={currentPage === 1}
+        onClick={() => setCurrentPage(currentPage - 1)}
+      />
+      {new Array(numOfPages).fill(numOfPages).map((_, index) => (
+        <Button
+          disabled={index + 1 === currentPage}
+          key={index}
+          onClick={() => setCurrentPage(index + 1)}
+        >
+          {index + 1}
+        </Button>
+      ))}
+      <IconButton
+        aria-label="next page"
+        icon={<FaCaretRight />}
+        disabled={currentPage === numOfPages}
+        onClick={() => setCurrentPage(currentPage + 1)}
+      />
+    </Flex>
+    {/* pagination controls END */}
   </>;
 };
