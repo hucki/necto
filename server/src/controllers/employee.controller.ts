b3acfdb51ada584ee386eff39521d8a09cb9b8ba
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-06-26 18:46:44 +0200

    feat(employee.controller): add update employee method

--- server/src/controllers/employee.controller.ts
+++ server/src/controllers/employee.controller.ts
@@ -81,3 +81,48 @@ export const addEmployee = async (
     next(e);
   }
 };
+
+/**
+ * update one Employee
+ *  @param {Employee} req.body
+ */
+ export const updateEmployee = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const updatedEmployee = await prisma.employee.update({
+      where: {
+        uuid: req.body.uuid,
+      },
+      data: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        alias: req.body.alias,
+        tenantId: tenantId,
+      },
+    });
+    // let updatedSettings;
+    // if (req.body.userId && req.body.userId !== req.body.user?.userId) {
+    //   updatedSettings = await prisma.userSettings.upsert({
+    //     where: {
+    //       id: req.body.user.id,
+    //     },
+    //     update: {
+    //       userId: req.body.userId,
+    //     },
+    //     create: {
+    //       userId: req.body.userId,
+    //       employeeId: req.body.uuid,
+    //       tenantId: tenantId,
+    //     },
+    //   });
+    // }
+    res.json({ updatedEmployee });
+    res.status(201);
+    return;
+  } catch (e) {
+    next(e);
+  }
+};
