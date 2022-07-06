49f8ac7c7eab4b4eb84f4e534fdb54daa9264bd4
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-07-06 09:04:55 +0200

    feat(permissions): new hook

---
+++ client/src/hooks/permissions.ts
@@ -0,0 +1,19 @@
+import {
+  useQuery,
+  QueryResult,
+} from 'react-query';
+import { client } from '../services/ApiClient';
+import { PermissionLevel } from '../types/UserSettings';
+
+export function useAllPermissions(): QueryResult<PermissionLevel[]> & { permissions: PermissionLevel[] } {
+  const permissionsQuery = useQuery('settings/permissions', async () => {
+    return client<PermissionLevel[]>('settings/permissions');
+  });
+
+  const permissions = permissionsQuery.data ?? [];
+
+  return {
+    permissions,
+    ...permissionsQuery,
+  };
+}
