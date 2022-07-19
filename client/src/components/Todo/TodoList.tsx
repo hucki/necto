555897cf6d1e617c00422ce1733ff853f93b35f4
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-07-19 20:34:28 +0200

    style(TodoList): style Todos

--- client/src/components/Todo/TodoList.tsx
+++ client/src/components/Todo/TodoList.tsx
@@ -1,15 +1,26 @@
+import { Box, Checkbox, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { FaTrash } from 'react-icons/fa';
+import { IconButton } from '../Library';
 
-const todos = [
+type Todo = {
+  id: string
+  todo: string
+  isCompleted: boolean
+  isDeleted: boolean
+  isDue: string
+}
+
+const todos: Todo[] = [
   {
-    id: 1,
+    id: '1',
     todo: 'do this',
     isCompleted: false,
     isDeleted: false,
     isDue: 'tomorrow'
   },
   {
-    id: 2,
+    id: '2',
     todo: 'do that',
     isCompleted: true,
     isDeleted: false,
@@ -17,12 +28,35 @@ const todos = [
   }
 ];
 
-const TodoList = () => {
-  const todoItems = todos.map(t => <li key={t.id}>{t.todo}</li>);
+
+
+type TodoItemProps = {
+  todo: Todo
+}
+
+const TodoItem = ({todo}: TodoItemProps) => {
   return <>
-    <ul>
+    <Box
+      textDecoration={todo.isDeleted ? 'line-through' : undefined}
+      alignItems="flex-start"
+      p="1"
+    >
+      <Checkbox isChecked={todo.isCompleted}>{todo.todo}</Checkbox>
+      <IconButton
+        size="sm"
+        aria-label="delete todo"
+        icon={<FaTrash />}
+      />
+    </Box>
+  </>;
+};
+
+const TodoList = () => {
+  const todoItems = todos.map(t => <TodoItem todo={t} key={t.id} />);
+  return <>
+    <VStack>
       {todoItems}
-    </ul>
+    </VStack>
   </>;
 };
 
