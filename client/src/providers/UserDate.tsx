d058eed5aada73bde71cfaeaa9843387c4d2979d
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-07-23 21:09:18 +0200

    feat(UserDateProvider): add goTO Method

--- client/src/providers/UserDate.tsx
+++ client/src/providers/UserDate.tsx
@@ -5,19 +5,44 @@ import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 dayjs.extend(LocalizedFormat);
 dayjs.locale('de');
 
+export type GoToTarget = 'previousDay' | 'previousWeek' | 'nextDay' | 'nextWeek'
+
 type UserDateContextType = {
   currentDate: Dayjs | undefined,
   setCurrentDate: Dispatch<SetStateAction<Dayjs>>,
+  goTo: (target: GoToTarget) => void
 }
+
 const UserDateContext = createContext<UserDateContextType>({
   currentDate: undefined,
   setCurrentDate: () => undefined,
+  goTo: (target) => undefined,
 });
 
 function UserDateProvider({children}:{children: any}) {
 
   const [currentDate, setCurrentDate] = useState(dayjs());
-  const value = useMemo(() => ({ currentDate, setCurrentDate}),[currentDate]);
+
+  const goTo = (target: GoToTarget) => {
+    switch (target) {
+      case 'previousWeek':
+        setCurrentDate(dayjs(currentDate).subtract(1, 'week'));
+        break;
+      case 'previousDay':
+        setCurrentDate(dayjs(currentDate).subtract(1, 'day'));
+        break;
+      case 'nextWeek':
+        setCurrentDate(dayjs(currentDate).add(1, 'week'));
+        break;
+      case 'nextDay':
+        setCurrentDate(dayjs(currentDate).add(1, 'day'));
+        break;
+      default:
+        break;
+    };
+  };
+
+  const value = useMemo(() => ({ currentDate, setCurrentDate, goTo}),[currentDate]);
 
   return (
     <UserDateContext.Provider value={value}>
