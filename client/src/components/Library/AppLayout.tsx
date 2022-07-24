e69c00de6b41abd2f259dc9e9b34e46c292b17a0
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-07-24 16:01:49 +0200

    feat(AppLayout): use new SideNav

--- client/src/components/Library/AppLayout.tsx
+++ client/src/components/Library/AppLayout.tsx
@@ -26,11 +26,34 @@ const Sidebar = styled.div(
     collapsed ? { width: '52px'} : { width: '200px', zIndex: '1', position: 'absolute'  }
 );
 
+interface SideNavContainerProps {
+  isOpen?: boolean;
+}
+const SideNavContainer = styled.div(
+  {
+    paddingTop: '0.5rem',
+    height: '100%', /* 100% Full-height */
+    width: '0', /* 0 width - change this with JavaScript */
+    position: 'fixed', /* Stay in place */
+    zIndex: '1', /* Stay on top */
+    top: '0', /* Stay at the top */
+    left: '0',
+    backgroundColor: '#fff',
+    overflowX: 'hidden', /* Disable horizontal scroll */
+    transition: '0.5s', /* 0.5 second transition effect to slide in the sidenav */
+    display: 'flex',
+    flexDirection: 'column',
+    rowGap: '0.5rem',
+    // transition: 'all 0.1s ease-in',
+  },
+  ({ isOpen }: SideNavContainerProps) =>
+    isOpen ? { width: '200px', zIndex: '1', position: 'absolute'  } : null
+);
+
 const ContentContainer = styled(Box)({
   width: '100%',
   height: '100%',
   display: 'flex',
-  overflow: 'scroll',
   flexDirection: 'column',
 });
 
@@ -46,6 +69,7 @@ const Header = styled(Box)({
 const Content = styled(Box)({
   width: '100%',
   height: '100%',
+  padding: '0 0.25rem 0 0.25rem'
 });
 
 const Footer = styled.div({
@@ -56,4 +80,4 @@ const Footer = styled.div({
   boxShadow: '0 0 15px #3333',
 });
 
-export { AppContainer, Sidebar, Header, Content, ContentContainer, Footer };
+export { AppContainer, Sidebar, SideNavContainer, Header, Content, ContentContainer, Footer };
