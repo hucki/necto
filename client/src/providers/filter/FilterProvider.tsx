0fbba3584735fb87ec223629fe812b8b3b6c2cd7
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-08-05 10:26:14 +0200

    feat(FilterProvider): preset company filter

--- client/src/providers/filter/FilterProvider.tsx
+++ client/src/providers/filter/FilterProvider.tsx
@@ -1,16 +1,25 @@
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { FunctionComponent, ReactNode, useEffect } from 'react';
 import { useState } from 'react';
 import { CalendarView } from './types';
 import { Company } from '../../types/Company';
 import { Team } from '../../types/Employee';
 import { filterContext } from '.';
+import { useAllCompanies } from '../../hooks/companies';
 
 export const FilterProvider: FunctionComponent = ({children}:  { children?: ReactNode; }) => {
-
+  const {
+    isLoading: isLoadingCompanies,
+    error: hasErrorCompanies,
+    companies,
+  } = useAllCompanies();
   const [ calendarView, setCalendarView ] = useState<CalendarView>('day');
-  const [ currentCompany, setCurrentCompany ] = useState<Company | undefined>(undefined);
+  const [ currentCompany, setCurrentCompany ] = useState<Company>(() => companies[0]);
   const [ currentTeam, setCurrentTeam ] = useState<Team | undefined>(undefined);
   const [ currentBuildingId, setCurrentBuildingId ] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    setCurrentCompany(companies[0]);
+  }, [companies]);
 
   return (
     <filterContext.Provider value={{
