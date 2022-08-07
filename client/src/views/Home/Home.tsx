5ad27856e03c4d06f15372919a1bce84bc9150f7
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-08-07 19:01:30 +0200

    feat(Home): only show NewUserPanel if authorized

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NewUserPanel from '../../components/InfoPanel/NewUserPanel';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const Home = () => {
+  const { user } = useContext(AuthContext);
+
   return <>
     <div className="home-wrapper" style={{padding: '0 0.25rem 0 0.25rem'}}>
-      <NewUserPanel />
+      {(user?.isAdmin || user?.isPlanner) && <NewUserPanel />}
     </div>
   </>;
 };
