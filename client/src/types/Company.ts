7a93e96670bd758c4475a12191a0c74778c03e5a
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-09-18 18:20:11 +0200

    feat(COmpany): add address to company type

--- client/src/types/Company.ts
+++ client/src/types/Company.ts
@@ -1,6 +1,10 @@
 export type Company = {
   uuid?: string;
   name: string;
+  name2: string;
+  street: string;
+  zip: string;
+  city: string;
   tenantId: string;
   validUntil: Date;
   createdAt?: Date;
