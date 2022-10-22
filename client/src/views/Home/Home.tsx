449d950349946a720d8fbb406099ef10aff79b1b
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-10-22 20:20:57 +0200

    feat(Home): add upcoming events panel

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -6,26 +6,79 @@ import {
   AccordionPanel,
   Box,
 } from '@chakra-ui/react';
+import dayjs from 'dayjs';
 import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 import ApprovalPanel from '../../components/InfoPanel/ApprovalPanel';
 import NewUserPanel from '../../components/InfoPanel/NewUserPanel';
-import { NotificationCount } from '../../components/Library/Messages';
-import { useLeavesByStatus } from '../../hooks/events';
-import { useAllUsers } from '../../hooks/user';
+import UpcomingPanel from '../../components/InfoPanel/UpcomingPanel';
+import { Greeting, NotificationCount } from '../../components/Library/Messages';
+import { useEmployeeEvents, useLeavesByStatus } from '../../hooks/events';
+import { useAllUsers, useUser } from '../../hooks/user';
 import { AuthContext } from '../../providers/AuthProvider';
 
+interface EmployeeEventAccordionItemProps {
+  employeeId: string;
+  now: dayjs.Dayjs;
+}
+const EmployeeEventAccordionItem = ({
+  employeeId,
+  now,
+}: EmployeeEventAccordionItemProps) => {
+  const { employeeEvents } = useEmployeeEvents(employeeId);
+  const upcomingEvents = employeeEvents.filter((e) =>
+    dayjs(e.startTime).isAfter(now)
+  );
+  return (
+    <AccordionItem isDisabled={upcomingEvents.length < 1}>
+      <h2>
+        <AccordionButton>
+          <Box flex="1" textAlign="left" display="flex" flexDirection="row">
+            <span>anstehende Ereignisse</span>
+            <NotificationCount count={upcomingEvents.length}>
+              {upcomingEvents.length}
+            </NotificationCount>
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>
+        <UpcomingPanel events={upcomingEvents} />
+      </AccordionPanel>
+    </AccordionItem>
+  );
+};
+
 const Home = () => {
+  const { t } = useTranslation();
   const { user } = useContext(AuthContext);
+  const { user: maxUser } = useUser(user?.uuid || '');
   const { users } = useAllUsers();
+  const employeeId =
+    (maxUser?.userSettings && maxUser?.userSettings[0].employeeId) || '';
+  const now = dayjs(new Date());
   const { rawEvents: events } = useLeavesByStatus('requested');
   const uniqueEvents = events.filter(
     (event) => event.rrule === '' || !event.parentEventId
   );
   const newUsers = users.filter((user) => !user?.permissions?.length);
+  const timeOfDay = now.isBetween(now.hour(0), now.hour(11), 'hour')
+    ? 'morning'
+    : now.isBetween(now.hour(10), now.hour(18), 'hour')
+    ? 'day'
+    : 'evening';
 
   return (
     <>
-      <div className="home-wrapper" style={{ padding: '0 0.25rem 0 0.25rem' }}>
+      <div
+        className="home-wrapper"
+        style={{
+          padding: '0 0.25rem 0 0.25rem',
+        }}
+      >
+        <Greeting>{`${t('dict.good')} ${t(`time.ofDay.${timeOfDay}`)} ${
+          user?.firstName
+        }`}</Greeting>
         <Accordion allowMultiple>
           {(user?.isAdmin || user?.isPlanner) && (
             <AccordionItem isDisabled={newUsers.length < 1}>
@@ -72,6 +125,9 @@ const Home = () => {
                 <ApprovalPanel events={uniqueEvents} />
               </AccordionPanel>
             </AccordionItem>
+          )}
+          {employeeId && (
+            <EmployeeEventAccordionItem employeeId={employeeId} now={now} />
           )}
         </Accordion>
       </div>
