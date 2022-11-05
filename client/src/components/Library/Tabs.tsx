bd12bcd285c4d3f69bb71302f016e01cd358e78e
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-11-05 19:27:51 +0100

    style(Tabs): change maxWidth

--- client/src/components/Library/Tabs.tsx
+++ client/src/components/Library/Tabs.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled/macro';
 import { TabPanel as ChakraTabPanel } from '@chakra-ui/react';
 
-const TabPanel = styled(ChakraTabPanel)({ maxWidth: '500px' });
+const TabPanel = styled(ChakraTabPanel)({ maxWidth: '1000px' });
 
 export { TabPanel };
