78191687a0be27d7b58e4d73f98f05a4e74c0158
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-11-24 08:45:59 +0100

    feat(Doctors): use FullPageSpinner

--- client/src/views/Doctors/Doctors.tsx
+++ client/src/views/Doctors/Doctors.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Spinner } from '@chakra-ui/react';
 import PersonList from '../../components/organisms/Person/PersonList';
 import { useAllDoctors } from '../../hooks/doctor';
 import { PersonListWrapper } from '../../components/atoms/Wrapper';
+import { FullPageSpinner } from '../../components/atoms/LoadingSpinner';
 
 function Doctors(): JSX.Element {
   const { isLoading, doctors } = useAllDoctors();
 
   return isLoading ? (
-    <Spinner />
+    <FullPageSpinner />
   ) : !doctors.length ? (
     <div>no data available</div>
   ) : (
