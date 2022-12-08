aeb73ae92b198e9c0d2c3f0d6dd5156cca57d613
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-12-08 20:52:40 +0100

    feat(LiveIndicator): new atom

---
+++ client/src/components/atoms/Indicator/Live.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled/macro';
+
+export const LiveIndicator = styled.div({
+  padding: '0.3rem',
+  margin: '0 0.5rem',
+  color: '#fff',
+  textDecoration: 'underline',
+});
