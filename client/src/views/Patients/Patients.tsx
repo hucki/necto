a385553993feae855f0a1180ca354c2403cb33cf
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-12-22 18:57:01 +0100

    feat(Patient.View): handle showArchived

--- client/src/views/Patients/Patients.tsx
+++ client/src/views/Patients/Patients.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PersonList from '../../components/organisms/Person/PersonList';
-import { useAllPatients } from '../../hooks/patient';
+import { useAllArchivedPatients, useAllPatients } from '../../hooks/patient';
 import { PersonListWrapper } from '../../components/atoms/Wrapper';
 import { FullPageSpinner } from '../../components/atoms/LoadingSpinner';
 
 function Patients(): JSX.Element {
   const { isLoading, patients } = useAllPatients();
-
-  return isLoading ? (
+  const { isLoading: isLoadingArchivedPatients, archivedPatients } =
+    useAllArchivedPatients();
+  const [showArchived, setShowArchived] = useState(false);
+  const isPending = isLoading || isLoadingArchivedPatients;
+  return isPending ? (
     <FullPageSpinner />
   ) : (
     <PersonListWrapper>
-      <PersonList persons={patients} listType="patients" />
+      <PersonList
+        persons={showArchived ? archivedPatients : patients}
+        listType="patients"
+        showArchived={showArchived}
+        setShowArchived={setShowArchived}
+      />
     </PersonListWrapper>
   );
 }
