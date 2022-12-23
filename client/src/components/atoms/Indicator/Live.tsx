26f209333b1334a3b07e73bc39f1e7d65049d67e
Stefan Huckschlag <stefan.huckschlag@web.de>
2022-12-23 12:57:04 +0100

    refactor(LiveIndicator): removed

--- client/src/components/atoms/Indicator/Live.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled/macro';
-
-export const LiveIndicator = styled.div({
-  padding: '0.3rem',
-  margin: '0 0.5rem',
-  color: '#fff',
-  textDecoration: 'underline',
-});
