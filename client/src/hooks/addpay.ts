b9a3ae82ee5856ac837c998fec13b030c16a5c47
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-01-13 11:11:35 +0100

    feat(hooks): new addpay hook to handle addpayFreedom CRUD

---
+++ client/src/hooks/addpay.ts
@@ -0,0 +1,71 @@
+import {
+  useMutation,
+  UseMutationResult,
+  useQuery,
+  useQueryClient,
+  UseQueryResult,
+} from '@tanstack/react-query';
+import { client } from '../services/ApiClient';
+import { AddpayFreedom } from '../types/Patient';
+
+export function useAddpayFreedomOfPatient(patientId: string): UseQueryResult<
+  AddpayFreedom[]
+> & {
+  addpayFreedom: AddpayFreedom[];
+} {
+  const addpayFreedomQuery = useQuery(['addpay'], async () => {
+    return client<AddpayFreedom[]>(`addpay/${patientId}`);
+  });
+
+  const addpayFreedom = addpayFreedomQuery.data ?? [];
+
+  return {
+    addpayFreedom,
+    ...addpayFreedomQuery,
+  };
+}
+
+export function useCreateAddpayFreedom(): UseMutationResult<
+  AddpayFreedom,
+  Error,
+  { addpayFreedom: AddpayFreedom },
+  string
+> {
+  const queryClient = useQueryClient();
+  const createAddpayFreedom = async ({
+    addpayFreedom,
+  }: {
+    addpayFreedom: AddpayFreedom;
+  }): Promise<AddpayFreedom> => {
+    return client<AddpayFreedom>('addpay', { data: addpayFreedom });
+  };
+  return useMutation(createAddpayFreedom, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['addpay']);
+    },
+  });
+}
+
+export function useDeleteAddpayFreedom(): UseMutationResult<
+  { message: string },
+  Error,
+  { uuid: string },
+  string
+> {
+  const queryClient = useQueryClient();
+  const deleteAddpayFreedom = async ({
+    uuid,
+  }: {
+    uuid: string;
+  }): Promise<{ message: string }> => {
+    return client<{ message: string }>(`addpay/${uuid}`, {
+      method: 'DELETE',
+    });
+  };
+
+  return useMutation(deleteAddpayFreedom, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['addpay']);
+    },
+  });
+}
