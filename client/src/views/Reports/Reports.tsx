92588ebc316215f791762410a705dd4a0837faff
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-02-05 21:18:34 +0100

    feat(Reports): use TabList

--- client/src/views/Reports/Reports.tsx
+++ client/src/views/Reports/Reports.tsx
@@ -1,10 +1,15 @@
+import { Tab, TabList, TabPanels, Tabs } from '@chakra-ui/react';
 import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { IoPeople } from 'react-icons/io5';
 import { FullPageSpinner } from '../../components/atoms/LoadingSpinner';
-import EmployeeReport from '../../components/organisms/List/EmployeeReport';
+import { TabPanel } from '../../components/Library';
+import EmployeeDashboard from '../../components/organisms/Dashboard/EmployeeDashboard';
 import { useAllEmployeesWithWeeksEvents } from '../../hooks/employees';
 import { UserDateContext } from '../../providers/UserDate';
 
 const Reports = () => {
+  const { t } = useTranslation();
   const { currentDate } = useContext(UserDateContext);
   const { data, isLoading } = useAllEmployeesWithWeeksEvents(
     currentDate.year(),
@@ -14,11 +19,39 @@ const Reports = () => {
     currentDate.startOf('week').format('DD.MM.') +
     ' - ' +
     currentDate.endOf('week').format('DD.MM.YY');
+
+  const tabData = [
+    {
+      allowedRoles: ['admin', 'planner', 'employee'],
+      name: 'teamSettings',
+      label: (
+        <>
+          <IoPeople /> {t('menu.teamSettings')}
+        </>
+      ),
+      content: (
+        <>
+          current week: {dateRange + ' (KW ' + currentDate.week() + ')'}
+          {(!data || isLoading) && <FullPageSpinner />}
+          {data && <EmployeeDashboard employees={data} />}
+        </>
+      ),
+    },
+  ];
   return (
     <>
-      current week: {dateRange + ' (KW ' + currentDate.week() + ')'}
-      {(!data || isLoading) && <FullPageSpinner />}
-      {data && <EmployeeReport employees={data} dateRange={dateRange} />}
+      <Tabs isLazy>
+        <TabList>
+          {tabData.map((tab, index) => {
+            return <Tab key={index}>{tab.label}</Tab>;
+          })}
+        </TabList>
+        <TabPanels>
+          {tabData.map((tab, index) => {
+            return <TabPanel key={index}>{tab.content}</TabPanel>;
+          })}
+        </TabPanels>
+      </Tabs>
     </>
   );
 };
