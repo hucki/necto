7c760bb69290e6bfbff5749fb14892f2f10195d8
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-03-24 11:13:48 +0100

    feat(Icons): add leaveType EventIcons

--- client/src/components/molecules/DataDisplay/Icons.tsx
+++ client/src/components/molecules/DataDisplay/Icons.tsx
@@ -1,16 +1,23 @@
 import { Icon } from '@chakra-ui/react';
 import React from 'react';
+import { IconType } from 'react-icons';
 import {
+  FaBaby,
   FaCommentMedical,
   FaExclamation,
   FaHouseUser,
   FaLink,
   FaPlane,
   FaPlaneDeparture,
+  FaQuestion,
+  FaSchool,
+  FaUmbrellaBeach,
 } from 'react-icons/fa';
+import { LeaveType } from '../../../types/Event';
 
 interface EventIconProps {
   type:
+    | LeaveType
     | 'homeVisit'
     | 'diagnostic'
     | 'noContract'
@@ -26,11 +33,20 @@ const sizes = {
   m: '1rem',
   l: '1.5rem',
 };
-
-const icons = {
+type EventIcons = {
+  // eslint-disable-next-line no-unused-vars
+  [key in EventIconProps['type']]: IconType;
+};
+const icons: EventIcons = {
   vacationFirstDay: FaPlaneDeparture,
   vacation: FaPlane,
+  paidVacation: FaUmbrellaBeach,
+  unpaidLeave: FaQuestion,
+  parentalLeave: FaBaby,
+  training: FaSchool,
+  special: FaSchool,
   sick: FaCommentMedical,
+  sickChild: FaCommentMedical,
   homeVisit: FaHouseUser,
   diagnostic: FaCommentMedical,
   noContract: FaExclamation,
@@ -40,8 +56,14 @@ const icons = {
 const colors = {
   vacationFirstDay: 'green',
   vacation: 'green',
+  paidVacation: 'green',
+  unpaidLeave: 'blueviolet',
+  parentalLeave: 'blueviolet',
   sick: 'orangered',
+  sickChild: 'orangered',
   homeVisit: 'steelblue',
+  training: 'steelblue',
+  special: 'steelblue',
   diagnostic: 'seagreen',
   noContract: 'tomato',
   recurring: 'blueviolet',
