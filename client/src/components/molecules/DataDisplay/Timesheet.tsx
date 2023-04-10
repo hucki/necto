88312ce6ff28d34426c9ee18686557b802edae31
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-04-10 15:13:59 +0200

    feat(Timesheet): useReactTable

--- client/src/components/molecules/DataDisplay/Timesheet.tsx
+++ client/src/components/molecules/DataDisplay/Timesheet.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
-import { Contract, Employee } from '../../../types/Employee';
-import { useEmployeeEvents } from '../../../hooks/events';
+import { Contract } from '../../../types/Employee';
 import dayjs from 'dayjs';
 import { useTranslation } from 'react-i18next';
-import { useHolidays } from '../../../hooks/useHolidays';
+import styled from '@emotion/styled/macro';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { EventsOfDay, TimesheetDay } from '../../organisms/Employee/SingleView';
+
+const Styles = styled.div`
+  padding: 0.5rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.25rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
 
 type TimesheetProps = {
   year: number;
@@ -11,92 +46,80 @@ type TimesheetProps = {
    * zero based month -> 0 = January
    */
   month: number;
-  employeeId: Employee['uuid'];
   contract: Contract;
+  currentTimesheet: TimesheetDay[];
 };
-const Timesheet = ({ year, month, employeeId, contract }: TimesheetProps) => {
-  const { data: events } = useEmployeeEvents(employeeId);
+const Timesheet = ({
+  year,
+  month,
+  contract,
+  currentTimesheet,
+}: TimesheetProps) => {
   const { t } = useTranslation();
-  const { isPublicHoliday, isWeekend } = useHolidays();
 
-  const filteredEvents = events?.filter(
-    (event) =>
-      (!event.isCancelled &&
-        dayjs(event.startTime).year() === year &&
-        dayjs(event.startTime).month() === month) ||
-      (dayjs(event.endTime).year() === year &&
-        dayjs(event.endTime).month() === month)
-  );
+  const columnHelper = createColumnHelper<TimesheetDay>();
 
-  const currentMonth = dayjs().year(year).month(month);
-  const daysInMonth = currentMonth.daysInMonth();
-  const monthName = currentMonth.format('MMM');
-
+  const columns = [
+    columnHelper.group({
+      id: 'day',
+      header: 'Day',
+      columns: [
+        columnHelper.accessor('dayOfMonth', {
+          header: 'Nr',
+          cell: (info) => info.getValue(),
+        }),
+        columnHelper.accessor('weekdayName', {
+          header: 'Tag',
+          cell: (info) => info.getValue(),
+        }),
+        columnHelper.accessor('targetTimeOfDay', {
+          header: 'Soll',
+          cell: (info) => info.getValue() || undefined,
+        }),
+        columnHelper.accessor('timeOfDay', {
+          header: 'Ist',
+          cell: (info) => info.getValue(),
+        }),
+        columnHelper.accessor('timeDiffOfDay', {
+          header: 'Diff',
+          cell: (info) => info.getValue(),
+        }),
+      ],
+    }),
+    columnHelper.group({
+      id: 'rest',
+      header: 'Info',
+      columns: [
+        columnHelper.accessor('publicHolidayName', {
+          cell: (info) => info.getValue(),
+        }),
+        columnHelper.accessor('eventsOfDay', {
+          cell: (info) => {
+            const eventsOfDay: EventsOfDay = info.getValue();
+            const hasLeaves = eventsOfDay?.leaves > 0;
+            return hasLeaves ? 'Abwesend' : undefined;
+          },
+        }),
+      ],
+    }),
+  ];
   const contractBase: 'hours' | 'appointments' =
-    contract.appointmentsPerWeek && contract.appointmentsPerWeek > 0
+    contract?.appointmentsPerWeek && contract?.appointmentsPerWeek > 0
       ? 'appointments'
       : 'hours';
 
-  const leaveWorthPerDay =
-    (contract[`${contractBase}PerWeek`] || 0) / contract.workdaysPerWeek;
+  const targetTimePerDay =
+    ((contract && contract[`${contractBase}PerWeek`]) || 0) /
+    ((contract && contract.workdaysPerWeek) || 0);
+  const leaveWorthPerDay = targetTimePerDay;
 
-  const rows = [];
-  for (let i = 1; i <= daysInMonth; i++) {
-    const eventsOfDay = filteredEvents
-      ?.filter(
-        (event) =>
-          dayjs(event.startTime).isSame(`${year}-${month + 1}-${i}`, 'day') ||
-          dayjs(event.endTime).isSame(`${year}-${month + 1}-${i}`, 'day')
-      )
-      .reduce(
-        (prev, cur) => {
-          if (cur.leaveType) {
-            prev.leaves++;
-          }
-          if (!cur.leaveType) {
-            prev.appointments++;
-            prev.hours += dayjs(cur.endTime).diff(
-              dayjs(cur.startTime),
-              'hours'
-            );
-          }
-          return prev;
-        },
-        { leaves: 0, hours: 0, appointments: 0 }
-      );
-
-    // .map((event) => (
-    //   <div key={event.uuid}>
-    //     {dayjs(event.startTime).toISOString() +
-    //       ' - ' +
-    //       dayjs(event.endTime).toISOString()}
-    //   </div>
-    // ));
-    const date = dayjs(`${year}-${month + 1}-${i}`);
-    const weekDay = date.format('dd');
-    const publicHoliday = isPublicHoliday({
-      date,
-    });
-
-    rows.push(
-      <div
-        key={i}
-        style={{
-          backgroundColor: isWeekend({
-            date,
-          })
-            ? 'lightgray'
-            : publicHoliday
-            ? 'lightskyblue'
-            : undefined,
-        }}
-      >
-        <b>{i}</b> ({weekDay} {publicHoliday && ' / ' + publicHoliday.join()}){' '}
-        <pre>{JSON.stringify(eventsOfDay)}</pre>
-      </div>
-    );
-  }
-
+  const currentMonth = dayjs().year(year).month(month);
+  const monthName = currentMonth.format('MMM');
+  const table = useReactTable({
+    data: currentTimesheet,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
   return (
     <>
       <div className="header">
@@ -114,7 +137,46 @@ const Timesheet = ({ year, month, employeeId, contract }: TimesheetProps) => {
           })}
         </div>
       </div>
-      {rows}
+      <Styles>
+        <table>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr
+                key={row.id}
+                style={{
+                  backgroundColor: row.original.isWeekend
+                    ? 'lightgray'
+                    : row.original.publicHolidayName
+                    ? 'lightskyblue'
+                    : undefined,
+                }}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Styles>
     </>
   );
 };
