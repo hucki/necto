b915684df4b2cef2e31852dfb3d295227d39429a
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-04-28 11:43:33 +0200

    feat(RoomCalendar): combine adjacent bookings

--- client/src/views/RoomCalendar/RoomCalendar.tsx
+++ client/src/views/RoomCalendar/RoomCalendar.tsx
@@ -13,9 +13,67 @@ import { Room } from '../../types/Rooms';
 import { useWeeksEvents } from '../../hooks/events';
 import { UserDateContext } from '../../providers/UserDate';
 import { FullPageSpinner } from '../../components/atoms/LoadingSpinner';
+import { Employee } from '../../types/Employee';
 
-function getRoomBookingsFromEvents(events: Event[]) {
-  return events
+function combineRoomBookings(events: Event[]): Event[] {
+  const eventsPerEmployee: {
+    employyeeId: Employee['uuid'];
+    events: Event[];
+  }[] = [];
+  for (let i = 0; i < events.length; i++) {
+    const currentList = eventsPerEmployee.find(
+      (list) => list.employyeeId === events[i].employee?.uuid
+    );
+    if (currentList) {
+      currentList.events.push(events[i]);
+    } else {
+      eventsPerEmployee.push({
+        employyeeId: events[i].employee?.uuid || '',
+        events: [events[i]],
+      });
+    }
+  }
+  for (let i = 0; i < eventsPerEmployee.length; i++) {
+    eventsPerEmployee[i] = {
+      ...eventsPerEmployee[i],
+      events: eventsPerEmployee[i].events
+        .sort((a, b) =>
+          a.startTime < b.startTime ? -1 : a.startTime === b.startTime ? 0 : 1
+        )
+        .reduce((prev, cur, i) => {
+          if (i === 0) {
+            prev.push(cur);
+          } else {
+            const lastIndex = prev.length - 1;
+            if (
+              !dayjs(cur.startTime).isSame(
+                dayjs(prev[lastIndex].startTime),
+                'day'
+              )
+            ) {
+              prev.push(cur);
+            } else if (
+              dayjs(cur.startTime).diff(
+                dayjs(prev[lastIndex].endTime),
+                'minutes'
+              ) < 30
+            ) {
+              prev[lastIndex].endTime = cur.endTime;
+            } else {
+              prev.push(cur);
+            }
+          }
+          return prev;
+        }, [] as Event[]),
+    };
+  }
+  return eventsPerEmployee.reduce((prev, cur) => {
+    prev.push(...cur.events);
+    return prev;
+  }, [] as Event[]);
+}
+function getRoomBookingsFromEvents(events: Event[]): Event[] {
+  const res = events
     .filter((event) => event.roomId)
     .map((event) => ({
       ...event,
@@ -24,6 +82,7 @@ function getRoomBookingsFromEvents(events: Event[]) {
       bgColor: event.employee?.contract[0].bgColor,
       type: 'roomBooking' as EventType,
     }));
+  return combineRoomBookings(res);
 }
 
 function getRooms(buildingId: string, rooms: Room[]) {
