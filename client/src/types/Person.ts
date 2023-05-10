4451b41acd2e23e8abef7da6fbc2791e27e08418
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-05-10 19:25:54 +0200

    feat(Person.types): add typeguards

--- client/src/types/Person.ts
+++ client/src/types/Person.ts
@@ -2,3 +2,18 @@ import { Doctor } from './Doctor';
 import { Patient, WaitingPatient } from './Patient';
 
 export type Person = Doctor | Patient | WaitingPatient;
+
+export const isWaitingPatient = (person: Person): person is WaitingPatient => {
+  if ('numberInLine' in person) return true;
+  return false;
+};
+
+export const isPatient = (person: Person): person is Patient => {
+  if ('firstContactAt' in person) return true;
+  return false;
+};
+
+export const isDoctor = (person: Person): person is Doctor => {
+  if (!isWaitingPatient(person) && !isPatient(person)) return true;
+  return false;
+};
