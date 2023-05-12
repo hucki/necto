d310bcb3cb29b9818b9b3fb43c55d89a8bf1a194
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-05-12 09:17:08 +0200

    feat(AddPayForm): show whole tag cursor pointer if isInteractive

--- client/src/components/organisms/Patients/AddpayForm.tsx
+++ client/src/components/organisms/Patients/AddpayForm.tsx
@@ -81,9 +81,10 @@ export const AddpayTags = ({
         variant={isActive ? 'solid' : 'subtle'}
         colorScheme={isActive ? 'green' : 'gray'}
         onClick={isInteractive ? clickHandler : undefined}
+        style={{ cursor: isInteractive ? 'pointer' : undefined }}
       >
         {isInteractive && clickHandler && !isActive && !yearsState.disabled && (
-          <TagLeftIcon as={FaPlus} style={{ cursor: 'pointer' }} />
+          <TagLeftIcon as={FaPlus} />
         )}
         <TagLabel style={{ textAlign: 'center' }}>{yearsState.year}</TagLabel>
 
