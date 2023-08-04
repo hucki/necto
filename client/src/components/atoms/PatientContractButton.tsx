6cd4f5b98a6877f24a7d724e41921b8248734f93
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-08-04 17:17:21 +0200

    feat(PatientContractButton): use PDFDownloadLink

--- client/src/components/atoms/PatientContractButton.tsx
+++ client/src/components/atoms/PatientContractButton.tsx
@@ -1,47 +1,55 @@
 import React from 'react';
-import { usePDF } from '@react-pdf/renderer';
+import { PDFDownloadLink } from '@react-pdf/renderer';
 import { Contract } from '../organisms/Documents/Contract';
-import { RiPrinterLine } from 'react-icons/ri';
 import { Patient } from '../../types/Patient';
 import dayjs from 'dayjs';
 import { Company } from '../../types/Company';
-import { IconButton } from './Buttons';
+import { Box } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 
 type PatientContractButtonProps = {
+  isDisabled: boolean;
   patient: Patient;
   company: Company;
   handleReset: () => void;
 };
 
 export const PatientContractButton = ({
+  isDisabled,
   patient,
   company,
   handleReset,
 }: PatientContractButtonProps) => {
+  const { t } = useTranslation();
+  const PrintBox = ({ isDisabled }: { isDisabled: boolean }) => {
+    return (
+      <Box
+        borderRadius="md"
+        bg={isDisabled ? 'lightslategrey' : 'darkolivegreen'}
+        color={isDisabled ? 'darkolivegreen' : 'white'}
+        h={10}
+        w={24}
+        p={2}
+        textAlign="center"
+        cursor={isDisabled ? 'not-allowed' : 'pointer'}
+      >
+        {t('label.printContract')}
+      </Box>
+    );
+  };
   const contractFileName = `vertrag_${(
     patient.lastName +
     '_' +
     patient.firstName
   ).replace(' ', '')}_${dayjs().format('YYYYMMDD')}.pdf`;
 
-  const [instance] = usePDF({
-    document: <Contract p={patient} c={company} />,
-  });
-
-  if (instance.error) return <div>Something went wrong: {instance.error}</div>;
-
   return (
-    <>
-      <IconButton
-        disabled={instance.loading || !instance.url}
-        aria-label="download"
-        href={instance.url || ''}
-        as="a"
-        icon={<RiPrinterLine />}
-        download={contractFileName}
-        colorScheme="blue"
-        onClick={handleReset}
-      />
-    </>
+    <PDFDownloadLink
+      fileName={contractFileName}
+      document={<Contract p={patient} c={company} />}
+      onClick={handleReset}
+    >
+      <PrintBox isDisabled={isDisabled} />
+    </PDFDownloadLink>
   );
 };
