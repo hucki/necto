6f622539c3424544b0695c2fcef5c5e95949c3cb
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-10-07 21:44:09 +0100

    feat(prisma): log connection result

--- server/src/db/prisma.ts
+++ server/src/db/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+prisma
+  .$connect()
+  .then(() => console.log('✅ DB connected!'))
+  .catch((error) => console.log('❌ DB connection failed:', error));
 
 export default prisma;
