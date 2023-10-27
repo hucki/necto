b43d9c77e83ccf77bafd8feff695f334bdc4ae44
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-10-27 23:46:21 +0100

    feat(Api): add types

--- client/src/types/Api.ts
+++ client/src/types/Api.ts
@@ -1,11 +1,22 @@
 import { Event } from './Event';
+import { Patient } from './Patient';
 
-export interface APIData {
+export interface EventAPIData {
   type: string;
   attributes: Event;
 }
-export interface APIResponse {
-  data: APIData[];
+export interface PatientAPIData {
+  type: string;
+  attributes: Patient;
+}
+export interface EventAPIResponse {
+  data: EventAPIData[];
+  jsonapi: {
+    version: string;
+  };
+}
+export interface PatientAPIResponse {
+  data: PatientAPIData[];
   jsonapi: {
     version: string;
   };
