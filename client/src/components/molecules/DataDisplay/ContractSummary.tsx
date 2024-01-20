27ec3bb52ee0d671b0b16da37ca8aa750c622e0b
Stefan Huckschlag <stefan.huckschlag@web.de>
2024-01-20 02:28:14 +0100

    feat(ContractSummary): add Doc variant

--- client/src/components/molecules/DataDisplay/ContractSummary.tsx
+++ client/src/components/molecules/DataDisplay/ContractSummary.tsx
@@ -2,21 +2,30 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Contract, NewContract } from '../../../types/Employee';
 import dayjs from 'dayjs';
+import { Text, View } from '@react-pdf/renderer';
 
 type ContractSummaryProps = {
   contract: Contract | NewContract;
 };
-const ContractSummary = ({ contract }: ContractSummaryProps) => {
+
+const getContractBase = (
+  contract: Contract | NewContract
+): 'hours' | 'appointments' => {
+  return contract.appointmentsPerWeek && contract.appointmentsPerWeek > 0
+    ? 'appointments'
+    : 'hours';
+};
+
+const getTargetTimePerDay = (contract: Contract | NewContract) => {
+  return (
+    ((contract && contract[`${getContractBase(contract)}PerWeek`]) || 0) /
+    ((contract && contract.workdaysPerWeek) || 0)
+  );
+};
+export const ContractSummary = ({ contract }: ContractSummaryProps) => {
   const { t } = useTranslation();
-  const contractBase: 'hours' | 'appointments' =
-    contract?.appointmentsPerWeek && contract?.appointmentsPerWeek > 0
-      ? 'appointments'
-      : 'hours';
-
-  const targetTimePerDay =
-    ((contract && contract[`${contractBase}PerWeek`]) || 0) /
-    ((contract && contract.workdaysPerWeek) || 0);
-
+  const contractBase: 'hours' | 'appointments' = getContractBase(contract);
+  const targetTimePerDay = getTargetTimePerDay(contract);
   const activeWorkdays = contract.activeWorkdays.split(',');
   const activeWorkdayNames =
     activeWorkdays.length !== 5
@@ -50,4 +59,45 @@ const ContractSummary = ({ contract }: ContractSummaryProps) => {
   );
 };
 
-export default ContractSummary;
+export const ContractSummaryDoc = ({ contract }: ContractSummaryProps) => {
+  const { t } = useTranslation();
+  const contractBase: 'hours' | 'appointments' = getContractBase(contract);
+  const targetTimePerDay = getTargetTimePerDay(contract);
+  const activeWorkdays = contract.activeWorkdays.split(',');
+  const activeWorkdayNames =
+    activeWorkdays.length !== 5
+      ? activeWorkdays
+          .map((day) => dayjs().day(parseInt(day)).format('dddd'))
+          .join(', ')
+      : undefined;
+
+  const leaveWorthPerDay = targetTimePerDay;
+  return (
+    <View style={{ display: 'flex', flexDirection: 'column' }}>
+      <Text>
+        {t('employee.contract.summary', {
+          numberOfUnits: contract[`${contractBase}PerWeek`] || 0,
+          contractBase: t(`employee.contract.${contractBase}`),
+          workdaysPerWeek: contract.workdaysPerWeek,
+        })}
+      </Text>
+      <Text>
+        {activeWorkdayNames ? (
+          <>
+            {t('employee.contract.activeWorkdays', {
+              activeWorkdayNames,
+            })}
+          </>
+        ) : undefined}
+      </Text>
+      <Text>
+        {t('employee.contract.leaveWorthDescription', {
+          leaveWorthPerDay,
+          contractBase: t(`employee.contract.${contractBase}`),
+        })}
+      </Text>
+    </View>
+  );
+};
+
+// export default ContractSummary;
