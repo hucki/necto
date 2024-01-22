427581af29c99ceb740dc7a43d6350d9ac8227fd
Stefan Huckschlag <stefan.huckschlag@web.de>
2024-01-22 11:24:20 +0100

    feat(TimeSheetYear): remove controls

--- client/src/components/organisms/Employee/TimeSheetYear.tsx
+++ client/src/components/organisms/Employee/TimeSheetYear.tsx
@@ -1,10 +1,6 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Contract, Employee } from '../../../types/Employee';
-import { UserDateContext } from '../../../providers/UserDate';
 import { useTimesheet } from '../../../hooks/timesheet';
-import dayjs from 'dayjs';
-import { IconButton } from '../../atoms/Buttons';
-import { CgPushChevronLeft, CgPushChevronRight } from 'react-icons/cg';
 import { TimesheetYearDoc } from '../Documents/TimesheetDoc';
 import { FullPageSpinner } from '../../atoms/LoadingSpinner';
 
@@ -12,16 +8,15 @@ type TimeSheetYearProps = {
   employeeId: Employee['uuid'];
   name: string;
   contract: Contract;
+  year: string;
 };
 
 export const TimeSheetYear = ({
   employeeId,
   name,
   contract,
+  year,
 }: TimeSheetYearProps) => {
-  const { currentDate, goTo } = useContext(UserDateContext);
-  const year = (currentDate.year() || dayjs().year()).toString();
-
   const { getTimesheetPerYear, status } = useTimesheet({
     employeeId,
     year,
@@ -29,22 +24,6 @@ export const TimeSheetYear = ({
   const timesheet = getTimesheetPerYear();
   return (
     <>
-      <IconButton
-        colorScheme="blackAlpha"
-        marginRight={2}
-        aria-label="previous month"
-        leftIcon={<CgPushChevronLeft size="2rem" />}
-        onClick={() => goTo('previousYear')}
-      />
-      {currentDate.format('YYYY')}
-      <IconButton
-        colorScheme="blackAlpha"
-        marginLeft={2}
-        marginRight={2}
-        aria-label="next month"
-        leftIcon={<CgPushChevronRight size="2rem" />}
-        onClick={() => goTo('nextYear')}
-      />
       {(status !== 'success' || !timesheet) && <FullPageSpinner />}
       {timesheet && (
         <TimesheetYearDoc
