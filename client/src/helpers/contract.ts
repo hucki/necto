35ba4c19190909a4b3e375c2ffc127d286a20f39
Stefan Huckschlag <stefan.huckschlag@web.de>
2024-02-13 23:05:00 +0100

    feat(contract): get contract of current month

--- client/src/helpers/contract.ts
+++ client/src/helpers/contract.ts
@@ -1,4 +1,22 @@
+import dayjs from 'dayjs';
 import { Contract, Employee } from '../types/Employee';
 
 export const getCurrentContract = (employee: Employee): Contract =>
   employee.contract[0];
+
+export const getContractOfCurrentMonth = (
+  employee: Employee,
+  year: number,
+  month: number
+): Contract => {
+  const currentMonth = dayjs().year(year).month(month);
+  const contract = employee.contract.find(
+    (contract) =>
+      (dayjs(contract.validFrom).isBefore(currentMonth, 'month') ||
+        dayjs(contract.validFrom).isSame(currentMonth, 'month')) &&
+      (dayjs(contract.validUntil).isAfter(currentMonth, 'month') ||
+        dayjs(contract.validUntil).isSame(currentMonth, 'month'))
+  );
+  if (!contract) throw new Error('No contract found for current month');
+  return contract;
+};
